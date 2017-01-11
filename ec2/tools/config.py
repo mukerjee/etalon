51239c78b31739374d18e679aee31759ccc87db4
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-01-10 19:02:18 -0500

    migrating to cmu aws account

--- ec2/tools/config.py
+++ ec2/tools/config.py
@@ -2,13 +2,13 @@
 
 ami = 'ami-6869aa05'
 key = 'daehyeok-workstation rsa'
-sec_group = 'sg-13971669'
-instance_type = 'c4.8xlarge'
-subnet = 'subnet-54415920'
-elastic_id = 'eipalloc-bf0cd381'
-elastic_ip = 'ec2-34-196-30-45.compute-1.amazonaws.com'
+sec_group = 'sg-5c12ee20'
+instance_type = 't2.micro'
+subnet = 'subnet-e8348db3'
+elastic_id = 'eipalloc-3a833704'
+elastic_ip = 'ec2-34-193-91-214.compute-1.amazonaws.com'
 host_count = 2
 group_name = 'DC'
 
-spot = True
+spot = False
 spot_price = 0.50  # 50 cents
