724a1388088791f587c5a5868bbc2a1afc2b5c68
Daehyeok Kim <dkimcs@gmail.com>
2017-01-13 20:26:52 -0500

    update for hadoop installation

--- ec2/tools/config.py
+++ ec2/tools/config.py
@@ -7,7 +7,7 @@ instance_type = 'c4.8xlarge'
 subnet = 'subnet-e8348db3'
 elastic_id = 'eipalloc-3a833704'
 elastic_ip = 'ec2-34-193-91-214.compute-1.amazonaws.com'
-host_count = 64
+host_count = 4
 group_name = 'DC'
 
 spot = True
--- ec2/tools/startup_hosts.py
+++ ec2/tools/startup_hosts.py
@@ -13,6 +13,29 @@ sed -Ei 's/^(Defaults.*requiretty)/#\1/' /etc/sudoers
 # setup folders where code will live
 mkdir -p /home/ec2-user/dc
 chown ec2-user:ec2-user -R /home/ec2-user/dc
+# Setting Hadoop
+wget http://apache.mesi.com.ar/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz -P /home/ec2-user/
+tar xvzf /home/ec2-user/hadoop-2.7.3.tar.gz -C /home/ec2-user/
+
+sudo mkdir /usr/local/hadoop
+sudo mv /home/ec2-user/hadoop-2.7.3/* /usr/local/hadoop/
+sudo chown -R ec2-user /usr/local/hadoop
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.121.x86_64
+export HADOOP_INSTALL=/usr/local/hadoop
+export PATH=$PATH:\$HADOOP_INSTALL/bin
+export PATH=$PATH:\$HADOOP_INSTALL/sbin
+export HADOOP_MAPRED_HOME=\$HADOOP_INSTALL
+export HADOOP_COMMON_HOME=\$HADOOP_INSTALL
+export HADOOP_HDFS_HOME=\$HADOOP_INSTALL
+export YARN_HOME=\$HADOOP_INSTALL
+export HADOOP_COMMON_LIB_NATIVE_DIR=\$HADOOP_INSTALL/lib/native
+export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_INSTALL/lib\"
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop" >> /home/ec2-user/.bash_profile
+
+source /home/ec2-user/.bash_profile
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.121.x86_64" >> $HADOOP_CONF_DIR/hadoop-env.sh
 
 # add static route through switch
 SUBNET="172.31.16.0/20"
@@ -32,14 +55,17 @@ yum -y upgrade
 yum -y install emacs
 yum -y --enablerepo=epel install iperf iperf3
 yum -y install tcpdump
+yum -y install java-1.7.0-openjdk-devel.x86_64
+yum -y install python2
 yum -y install htop
 yum -y groupinstall "Development Tools"
 
 # fix netfilter
-sed -i 's/limits\.h/linux\/kernel\.h/g' /usr/src/kernels/4.4.19-29.55.amzn1.x86_64/include/uapi/linux/netfilter_ipv4.h
+#sed -i 's/limits\.h/linux\/kernel\.h/g' /usr/src/kernels/4.4.19-29.55.amzn1.x86_64/include/uapi/linux/netfilter_ipv4.h
 
 echo "PATH=~/dc/tools:\$PATH" >> /home/ec2-user/.bash_profile
-echo "/etc/init.d/add_route" >> /home/ec2-user/.bash_profile
+#echo "/etc/init.d/add_route" >> /home/ec2-user/.bash_profile
+
 
 reboot
 
