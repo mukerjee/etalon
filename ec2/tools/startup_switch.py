aec79f6190b1b60438829475059fb7a54fc75aea
Daehyeok Kim <dkimcs@gmail.com>
2017-01-14 20:07:12 -0500

    add yum install libboost for the switch

--- ec2/tools/startup_switch.py
+++ ec2/tools/startup_switch.py
@@ -62,7 +62,7 @@ yum -y install emacs
 yum -y --enablerepo=epel install iperf iperf3
 yum -y install tcpdump
 yum -y install iptables-devel
-yum -y install libnetfilter_queue-devel
+yum -y install libnetfilter_queue-devel boost-devel
 yum -y groupinstall "Development Tools"
 
 # fix netfilter
