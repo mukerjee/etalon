6aadbb69687e55efc7bb3f637c81ca03f1728c55
mukerjee <mukerjee@cs.cmu.edu>
2017-05-31 13:30:45 -0600

    updating boot scripts

--- cloudlab/node_startup.sh
+++ cloudlab/node_startup.sh
@@ -2,3 +2,10 @@
 
 sudo apt-get update
 sudo apt-get -y install iperf3 git zsh curl vim tmux python-pip xorg-dev libx11-dev htop git make g++ gcc emacs
+
+ping host0 -c1
+ping host1 -c1
+ping router -c1
+
+sudo arp -s host0 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
+sudo arp -s host1 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
