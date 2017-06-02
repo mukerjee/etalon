e9a13233799e772d1153348444625514470b9123
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-06-02 11:18:00 -0400

    5 node experiments startup script

--- cloudlab/node_startup.sh
+++ cloudlab/node_startup.sh
@@ -5,9 +5,13 @@ sudo apt-get -y install iperf3 git zsh curl vim tmux python-pip xorg-dev libx11-
 
 ping host0 -c1
 ping host1 -c1
+ping host2 -c1
+ping host3 -c1
 ping router -c1
 
 sudo arp -s host0 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
 sudo arp -s host1 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
+sudo arp -s host2 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
+sudo arp -s host3 `arp | grep router | tr -s ' ' | cut -d' ' -f3`
 
 #sudo tcpdump -nnXXSs 0 -i enp8s0d1
