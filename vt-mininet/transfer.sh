5fa06581f869cb2b35996e8f1a4d27352532aec3
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-09-01 20:44:59 -0400

    fixing up patching util

--- vt-mininet/transfer.sh
+++ vt-mininet/transfer.sh
@@ -2,7 +2,6 @@
 
 if [ "z$1" = "z" ]; then
     echo "usage: $0 path/to/kernel/src"
-    echo "  e.g. $0 /home/yjq/ExtendKernel/linux-3.16.3/linux-3.16.3"
     exit 1
 fi
 
@@ -13,22 +12,7 @@ if [ ! -e $DST ]; then
     exit 1
 fi
 
-FILES="kernel/fork.c \
-kernel/time.c \
-kernel/time/timekeeping.c \
-arch/x86/syscalls/syscall_64.tbl \
-arch/x86/vdso/vclock_gettime.c \
-include/uapi/asm-generic/unistd.h include/uapi/linux/sched.h \
-include/linux/sched.h \
-include/linux/init_task.h \
-include/linux/syscalls.h \
-include/net/sch_generic.h \
-net/sched/sch_generic.c \
-net/sched/sch_htb.c \
-net/sched/act_police.c \
-build_all.sh"
-
 echo "Step 1. transfer modified kernel source files"
-for f in $FILES; do
-    cp -v $f $DST/$f
+for f in $(find ./kernel_patches/* -name '*.patch'); do
+    patch -p1 < $f
 done
