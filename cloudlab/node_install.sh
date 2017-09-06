d122aaacc446f891e659c10693069166f2639737
Matthew Mukerjee <mukerjee@cs.cmu.edu>
2017-09-05 21:47:31 -0600

    small script fixes

--- cloudlab/node_install.sh
+++ cloudlab/node_install.sh
@@ -3,8 +3,7 @@
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
 sudo apt-get update
-sudo apt-get -y install iperf3 git zsh curl vim tmux python-pip xorg-dev libx11-dev htop git make g++ gcc emacs libcap-dev libidna11-dev nettle-dev
-sudo apt-get -y install autoconf automake libtool make gcc git socat psmisc xterm ssh iperf iperf3 iproute telnet python-setuptools cgroup-bin ethtool help2man pyflakes pylint pep8 git-core autotools-dev pkg-config libc6-dev python-numpy python-matplotlib
+sudo apt-get -y install iperf3 git zsh curl vim tmux python-pip xorg-dev libx11-dev htop git make g++ gcc emacs libcap-dev libidna11-dev nettle-dev autoconf automake libtool make gcc git socat psmisc xterm ssh iperf iperf3 iproute telnet python-setuptools cgroup-bin ethtool help2man pyflakes pylint pep8 git-core autotools-dev pkg-config libc6-dev python-numpy python-matplotlib
 
 cd $HOME
 git clone git://openflowswitch.org/openflow.git
@@ -12,8 +11,8 @@ cd $HOME/openflow
 sudo ./boot.sh
 sudo ./configure
 patch -p1 < $HOME/sdrt/vt-mininet/mininet/util/openflow-patches/controller.patch
-sudo make
-sudo make install
+sudo make -j16
+sudo make -j16 install
 
 sudo apt-get -y install openvswitch-switch
 sudo apt-get -y install openvswitch-controller
@@ -22,7 +21,7 @@ if [ -e /etc/init.d/openvswitch-controller ]; then
    sudo update-rc.d openvswitch-controller disable
 fi
 
-cd `mount | grep proj | cut -f3 -d' '`/linux-3.16.3-vtmininet
+cd `mount | grep proj | cut -f3 -d' '`/linux-3.16.3-vt-mininet
 # sudo make -j16
 sudo make -j16 headers_install
 # sudo make -j16 modules
