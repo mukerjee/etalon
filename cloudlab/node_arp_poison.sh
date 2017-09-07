6bedebeb97ed68943a6b22591979f7f5104d1fe0
Matthew Mukerjee <mukerjee@cs.cmu.edu>
2017-09-06 20:25:18 -0600

    8 nodes not 16

--- cloudlab/node_arp_poison.sh
+++ cloudlab/node_arp_poison.sh
@@ -4,13 +4,13 @@ h=`hostname | cut -d'.' -f1`
 sudo ifconfig enp8s0 10.10.1.`echo ${h:4} | awk '{print $1+2}'` netmask 255.255.255.0
 sudo ifconfig enp8s0d1 10.10.2.`echo ${h:4} | awk '{print $1+2}'` netmask 255.255.255.0
 
-for i in {0..15}
+for i in {0..7}
 do
     ping host$i -c1 -W1
 done
 ping router -c1 -W1
 
-for i in {0..15}
+for i in {0..7}
 do
     sudo arp -s host$i `arp | grep router | tr -s ' ' | cut -d' ' -f3`
 done
