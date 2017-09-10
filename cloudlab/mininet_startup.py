2f5e5626124f63dab7bc565bfe20fb21b522a74f
Matthew Mukerjee <mukerjee@cs.cmu.edu>
2017-09-10 14:59:15 -0600

    update mininet startup

--- cloudlab/mininet_startup.py
+++ cloudlab/mininet_startup.py
@@ -9,6 +9,7 @@ from mininet.node import CPULimitedHost
 from mininet.cli import CLI
 from mininet.link import Intf, Link
 from mininet.log import setLogLevel, info
+from mininet.util import waitListening
 
 host = int(gethostname().split('.')[0][4:])
 
@@ -20,35 +21,39 @@ def myNetwork():
 
     info('*** Add switches\n')
     s1 = net.addSwitch('s1')
-    Intf('eth1', node=s1)
+    Intf('eth2', node=s1)
     s2 = net.addSwitch('s2')
-    Intf('eth2', node=s2)
+    Intf('eth3', node=s2)
 
     info('*** Add hosts\n')
     h = net.addHost('h%d' % host, ip='10.10.1.%d/24' % (host+2), cls=CPULimitedHost,
                     sched='cfs', period_us=100000, cpu=1,
-                    tdf=10)
+                    tdf=1)
     
     info('*** Add links\n')
     Link(h, s1, intfName1='h%d-eth1' % host)
     Link(h, s2, intfName1='h%d-eth2' % host)
-    h.cmd('ifconfig h%d-eth2 10.10.2.%d netmask 255.255.255.0' % (host, host+4))
+    h.cmd('ifconfig h%d-eth2 10.10.2.%d netmask 255.255.255.0' % (host, host+2))
 
     info('*** Starting network\n')
     net.start()
+    info('*** done that\n')
     
-    for i in xrange(16):
-        h.cmd('ping 10.10.1.%d -c1 -W1' % (i+2));
-    h.cmd('ping router -c1 -W1')
+    # for i in xrange(16):
+    #     h.cmd('ping 10.10.1.%d -c1 -W1' % (i+2));
+    # h.cmd('ping router -c1 -W1')
 
-    for i in xrange(16):
-        h.cmd('arp -s 10.10.1.%d `arp | grep router | tr -s ' ' | cut -d' ' -f3`'
-              % (i+2))
+    # for i in xrange(16):
+    #     h.cmd('arp -s 10.10.1.%d `arp | grep router | tr -s ' ' | cut -d' ' -f3`'
+    #           % (i+2))
+
+    h.cmd('/usr/sbin/sshd -D &')
+    waitListening(server=h, port=22, timeout=5)
 
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
-    # subprocess.call([os.path.expanduser('~/sdrt/cloudlab/node_arp_poison.sh')])
+    subprocess.call([os.path.expanduser('~/sdrt/cloudlab/node_arp_poison.sh')])
     setLogLevel('info')    
     myNetwork()
