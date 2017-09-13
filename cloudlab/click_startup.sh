bc7e756fca5e554293d65be47ac044d725e293bd
Matthew Mukerjee <mukerjee@cs.cmu.edu>
2017-09-13 15:46:56 -0600

    make sure router talks to hosts via control channel if need be

--- cloudlab/click_startup.sh
+++ cloudlab/click_startup.sh
@@ -3,9 +3,11 @@
 
 $HOME/sdrt/cloudlab/tune.sh
 
+sudo sed -i -r 's/10.10.1.([[:digit:]][[:digit:]])/10.10.2.\1/' /etc/hosts
+
 # run click
 # n = 4? n = 3?
 # http://dpdk-guide.gitlab.io/dpdk-guide/tuning/memory.html
 # http://dpdk.org/doc/guides/nics/mlx4.html
 cd $HOME/sdrt/click/
-sudo nice -n -20 ./userlevel/click --dpdk -c 0xFFFF -n 4 --  ./conf/dpdk-hybrid-switch.click
+make -j16 && sudo nice -n -20 ./userlevel/click --dpdk -c 0xFFFF -n 4 --  ./conf/dpdk-hybrid-switch.click
