681d95b0c257a58c0441d558e116d4543ec0bbb4
Matthew Mukerjee <mukerjee@cs.cmu.edu>
2017-09-14 21:05:55 -0600

    rpyc maybe will fix some issues with iperf prints not forwarding over ssh

---
+++ cloudlab/rpyc_daemon.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import rpyc
+from subprocess import Popen, PIPE
+
+RPYC_PORT = 18861
+
+iperf3_client = 'iperf3 -i0.1 -t%s -c %s'
+ping = 'sudo ping -i0.05 -w%s %s'
+
+class MyService(rpyc.Service):
+    def on_connect(self):
+        pass
+
+    def on_disconnection(self):
+        pass
+
+    def exposed_iperf3(self, host, time=10):        
+        cmd = iperf3_client % (time, host)
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        sout, serr = p.communicate()
+        return p.returncode, sout, serr
+
+    def exposed_ping(self, host, time=10):        
+        cmd = ping % (time, host)
+        print cmd
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        sout, serr = p.communicate()
+        return p.returncode, sout, serr
+
+    def exposed_uname(self):
+        call
+
+if __name__ == '__main__':
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(MyService, port = RPYC_PORT)
+    t.start()
