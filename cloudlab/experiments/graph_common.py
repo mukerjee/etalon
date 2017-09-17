ed83c9418ebe7128679fdf11c265f8b64678ef8d
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-09-17 00:18:28 -0400

    graph scripts

--- cloudlab/experiments/graph_common.py
+++ cloudlab/experiments/graph_common.py
@@ -1,15 +1,22 @@
 import os
 import tarfile
 import glob
+import math
 import numpy as np
 
 
 def get_ping_from_file(fn):
-    ping_file = list(open(fn))[1:]
     lats = {}
     sn = 0
-    for line_space in ping_file:
+    ping_file = list(open(fn))
+    if len(ping_file) < 2 or 'bad: timeout' in ping_file[0] \
+       or 'bad RC' in ping_file[0]:
+        print 'bad timeout'
+        return lats
+    for line_space in ping_file[1:]:
         line = line_space.strip()
+        if 'bad: timeout' in line:
+            break
         if not line:
             break
         if line[0] == '-':
@@ -19,17 +26,28 @@ def get_ping_from_file(fn):
         sn = int(line.split('icmp_seq=')[1].split()[0])
         lat = float(line.split('time=')[1].split()[0])
         lats[sn] = lat * 1000.0  # usecs
-    max_sn = max(lats.keys())
-    lats = [l for (s, l) in lats.items() if s > 5 and s < max_sn - 5]
+    if lats:
+        max_sn = max(lats.keys())
+        lats = [l for (s, l) in lats.items() if s > 5 and s < max_sn - 5]
+    else:
+        print 'ping error'
     return lats
 
     
 def get_iperf3_tput_from_file(fn):
-    iperf3_file = list(open(fn))[3:]
     tps = []
-    for line in iperf3_file:
+    iperf3_file = list(open(fn))
+    if len(iperf3_file) < 4 or 'bad: timeout' in iperf3_file[0]:
+        print 'bad timeout'
+        return [0]
+    for line in iperf3_file[3:]:
+        if 'bad: timeout' in line:
+            print 'bad timeout'
+            break
         if line[0] == '-':
             break
+        # if 'sender' not in line:
+        #     continue
         l = line.split(']')[1].strip().split()
         tp = float(l[4])
         units = l[5]
@@ -42,39 +60,66 @@ def get_iperf3_tput_from_file(fn):
         elif units == 'bits/sec':
             tp = tp / 1000000000.0
         tps.append(tp)
-    return tps
+    if tps:
+        return tps
+    else:
+        return 0
 
 
 def untar(fn):
-    TMP = "/tmp/sdrt"
+    TMP = "/tmp/sdrt/%s" % (os.path.basename(fn).split('.tar.gz')[0])
     if not os.path.exists(TMP):
         os.makedirs(TMP)
-    dir = os.getcwd()
-    os.chdir(TMP)
-    tar = tarfile.open(dir + "/" + os.path.basename(fn))
-    tar.extractall()
-    tar.close()
-    os.chdir(dir)
+        dir = os.getcwd()
+        os.chdir(TMP)
+        tar = tarfile.open(dir + "/" + os.path.basename(fn))
+        tar.extractall()
+        tar.close()
+        os.chdir(dir)
 
 
 def get_tput_and_lat(pattern, ts, c):
     untar('%s-%s.tar.gz' % (ts, pattern))
     print c
-    tputs = []
-    pings = []
-    fn_format = '/tmp/sdrt/%s-%s-%s-%d-%s-%s-*' % (ts, pattern, c['type'],
-                                                   c['buffer_size'],
-                                                   c['traffic_source'],
-                                                   c['queue_resize'])
+    NUM_RACKS = 8  # grab num_racks from somewhere...
+    tputs = [[] for x in xrange(NUM_RACKS+1)]
+    tputs_down = [[] for x in xrange(NUM_RACKS+1)]
+    pings = [[] for x in xrange(NUM_RACKS+1)]
+    tail_pings = []
+    fn_format = '/tmp/sdrt/%s-%s/%s-%s-%s-%d-%s-%s-*' % (
+        ts, pattern, ts, pattern, c['type'], c['buffer_size'],
+        c['traffic_source'], c['queue_resize'])
     fn_format += '-%s.txt'
     ping_files = glob.glob(fn_format % 'ping')
     iperf3_files = glob.glob(fn_format % 'iperf3')
     for ip3fn in iperf3_files:
-        tputs.append(get_iperf3_tput_from_file(ip3fn))
+        src, dst = ip3fn.replace(fn_format.split('*')[0],
+                                 '').split('-iperf3')[0].split('-')
+        src_rack = int(src[1])
+        dst_rack = int(dst[1])
+        file_iperf_avg = np.average(get_iperf3_tput_from_file(ip3fn))
+        tputs[src_rack].append(file_iperf_avg)
+        tputs_down[dst_rack].append(file_iperf_avg)
     for pfn in ping_files:
-        pings.append(get_ping_from_file(pfn))
-    tputs = [np.median(host_tput) for host_tput in tputs]
-    tputs = (np.median(tputs), np.var(tputs))
-    pings = [np.median(host_ping) for host_ping in pings]
-    pings = (np.median(pings), np.var(tputs))
-    return tputs, pings
+        src, dst = pfn.replace(fn_format.split('*')[0],
+                               '').split('-ping')[0].split('-')
+        src_rack = int(src[1])
+        dst_rack = int(dst[1])
+        p = get_ping_from_file(pfn)
+        if p:
+            pings[src_rack].append(np.median(p))
+            tail_pings += p
+    for rack in xrange(NUM_RACKS+1):
+        tputs[rack] = (sum(tputs[rack]) / 80.0) * 100
+        tputs_down[rack] = (sum(tputs_down[rack]) / 80.0) * 100
+        pings[rack] = np.median(pings[rack])
+    tputs = filter(lambda a: a, tputs)
+    tputs_down = filter(lambda a: a, tputs_down)
+    pings = filter(lambda a: not math.isnan(a), pings)
+    # tputs = (np.median(tputs), np.std(tputs))
+    # tputs = (sum(tputs), np.std(tputs))
+    tputs = (np.average(tputs), np.std(tputs))
+    tputs_down = (np.average(tputs_down), np.std(tputs_down))
+    pings = (np.median(pings), np.std(pings))
+    tail_pings = (np.percentile(tail_pings, 90), np.percentile(tail_pings, 99))
+    return tputs, pings, tputs_down, tail_pings
