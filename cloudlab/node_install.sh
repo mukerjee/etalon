cf1961eb0724267601abf83168a07dd66c32e064
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-06 00:57:43 -0400

    more boot tweaks

--- cloudlab/node_install.sh
+++ cloudlab/node_install.sh
@@ -2,32 +2,34 @@
 
 OFED_VERSION=4.1-1.0.2.0
 
-(crontab -l 2>/dev/null; echo "@reboot $HOME/sdrt/cloudlab/tune.sh") | crontab -
+# get SDRT
+cd /local
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --recursive https://github.com/mukerjee/sdrt.git
+(crontab -l 2>/dev/null; echo "@reboot /local/sdrt/cloudlab/tune.sh") | crontab -
 
 sudo apt-get update && sudo apt-get install -y \
                             git
 
-
 # Mellanox OFED
 # http://www.mellanox.com/related-docs/prod_software/Mellanox_OFED_Linux_User_Manual_v4.0.pdf
-cd $HOME
+cd /local
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-$OFED_VERSION/MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64.tgz
 tar xfz ./MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64.tgz
 sudo ./MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64/mlnxofedinstall --force
 sudo /etc/init.d/openibd restart
 
 # get docker
-cd $HOME
+cd /local
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+for u in `cut -d: -f1 /etc/passwd`
+do
+    sudo usermod -aG docker $u
+done
 
 # get pipework
-cd $HOME
+cd /local
 sudo bash -c "curl https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework > /usr/local/bin/pipework"
 sudo chmod +x /usr/local/bin/pipework
-
-# get SDRT
-cd $HOME
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --recursive https://github.com/mukerjee/sdrt.git
 
 sudo reboot
--- cloudlab/switch_install.sh
+++ cloudlab/switch_install.sh
@@ -3,23 +3,24 @@
 OFED_VERSION=4.1-1.0.2.0
 DPDK_VERSION=16.11_2.3
 
-(crontab -l 2>/dev/null; echo "@reboot $HOME/sdrt/cloudlab/tune.sh") | crontab -
+# get SDRT
+cd /local
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --recursive https://github.com/mukerjee/sdrt.git
+(crontab -l 2>/dev/null; echo "@reboot local/sdrt/cloudlab/tune.sh") | crontab -
 
 sudo apt-get update && sudo apt-get install -y \
                             git
 
-echo "" >> $HOME/.bashrc
-
 # Mellanox OFED
 # http://www.mellanox.com/related-docs/prod_software/Mellanox_OFED_Linux_User_Manual_v4.0.pdf
-cd $HOME
+cd /local
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-$OFED_VERSION/MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64.tgz
 tar xfz ./MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64.tgz
 sudo ./MLNX_OFED_LINUX-$OFED_VERSION-ubuntu16.04-x86_64/mlnxofedinstall --force --dpdk
 
 # Mellanox DPDK
 # http://www.mellanox.com/related-docs/prod_software/MLNX_DPDK_Quick_Start_Guide_v16.11_2.3.pdf
-cd $HOME
+cd /local
 sudo connectx_port_config -c eth,eth
 echo 'options mlx4_core log_num_mgm_entry_size=-7' | sudo tee -a /etc/modprobe.d/mlx4.conf
 sudo /etc/init.d/openibd restart
@@ -30,25 +31,25 @@ make install T=x86_64-native-linuxapp-gcc
 
 # Huge pages
 # http://dpdk.org/doc/guides-16.04/linux_gsg/sys_reqs.html
-cd $HOME
+cd /local
 sudo sed -i -r 's/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 default_hugepagesz=1G hugepagesz=1G hugepages=4\"/' /etc/default/grub
 sudo update-grub
 sudo mkdir /mnt/huge_1GB
 echo 'nodev /mnt/huge_1GB hugetlbfs pagesize=1GB 0 0' | sudo tee -a /etc/fstab
 
-echo "" >> $HOME/.bashrc
-echo "export RTE_SDK=$HOME/MLNX_DPDK_$DPDK_VERSION" >> $HOME/.bashrc
-echo "export RTE_TARGET=x86_64-native-linuxapp-gcc" >> $HOME/.bashrc
-export RTE_SDK=$HOME/MLNX_DPDK_$DPDK_VERSION
+for f in /users/*/.bashrc
+do
+    echo "" | sudo tee -a f
+    echo "export RTE_SDK=/local/MLNX_DPDK_$DPDK_VERSION" | sudo tee -a f
+    echo "export RTE_TARGET=x86_64-native-linuxapp-gcc" | sudo tee -a f
+    echo "cd /local/" | sudo tee -a f
+fi
+export RTE_SDK=/local/MLNX_DPDK_$DPDK_VERSION
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 
 # make Click
-cd $HOME/sdrt/click
+cd /local/sdrt/click
 ./configure --enable-user-multithread --disable-linuxmodule --enable-intel-cpu --enable-nanotimestamp --enable-dpdk
 make -j
-
-# get SDRT
-cd $HOME
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --recursive https://github.com/mukerjee/sdrt.git
 
 sudo reboot
