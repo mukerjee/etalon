1e0f39fd5ddc8635f969c2db738bed5cdad61434
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-06 21:11:19 -0400

    since root isn't needed, don't need to be tricky

--- cloudlab/node_install.sh
+++ cloudlab/node_install.sh
@@ -3,9 +3,9 @@
 OFED_VERSION=4.1-1.0.2.0
 
 # get SDRT
-cd /local
+cd $HOME
 GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --recursive https://github.com/mukerjee/sdrt.git
-(crontab -l 2>/dev/null; echo "@reboot sleep 60 && /local/sdrt/cloudlab/tune.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 60 && $HOME/sdrt/cloudlab/tune.sh") | crontab -
 sudo rm /var/run/crond.reboot
 
 sudo apt-get update && sudo apt-get install -y \
@@ -27,23 +27,12 @@ sudo /etc/init.d/openibd restart
 cd /local
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-for u in `cut -d: -f1 /etc/passwd`
-do
-    sudo usermod -aG docker $u
-done
+sudo usermod -aG docker `whoami`
 
 # get pipework
-cd /local
 sudo bash -c "curl https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework > /usr/local/bin/pipework"
 sudo chmod +x /usr/local/bin/pipework
 
-# start in /local/sdrt/
-for f in /users/*/.bashrc
-do
-    echo "" | sudo tee -a $f
-    echo "cd /local/sdrt/" | sudo tee -a $f
-done
-
-systemctl enable /local/sdrt/cloudlab/rpyc_daemon.service
+sudo systemctl enable $HOME/sdrt/cloudlab/rpyc_daemon.service
 
 sudo reboot
