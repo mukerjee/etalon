a950a687435431da17dc945ed0cc6056584cf23b
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-17 14:01:43 -0400

    rack level

--- cloudlab/experiments/buffers/one_to_one.py
+++ cloudlab/experiments/buffers/one_to_one.py
@@ -16,7 +16,7 @@ for config in buffer_common.CONFIGS:
     print '--- done...'
 
     settings = {'flows': []}
-    settings['flows'].append({'src': 'h11', 'dst': 'h21'})
+    settings['flows'].append({'src': 'r1', 'dst': 'r2'})
     flowgrind(settings)
 
 tarExperiment()
