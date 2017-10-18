4fedf8b3e4af03d3535622612b5f967396002166
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-18 14:07:11 -0400

    typo 2

--- cloudlab/click_startup.sh
+++ cloudlab/click_startup.sh
@@ -5,4 +5,4 @@
 # http://dpdk-guide.gitlab.io/dpdk-guide/tuning/memory.html
 # http://dpdk.org/doc/guides/nics/mlx4.html
 ~/sdrt/click-utils/gen-switch.py > /tmp/hybrid-switch.click
-sudo nice -n -20 ./userlevel/click --dpdk -c 0xFFFF -n 4 --  /tmp/hybrid-switch.click
+sudo nice -n -20 click --dpdk -c 0xFFFF -n 4 --  /tmp/hybrid-switch.click
