be38095cd34513f470170f7137009112d185ff6e
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-19 16:50:36 -0400

    seeing if buffer resize works

--- cloudlab/experiments/buffers/buffer_common.py
+++ cloudlab/experiments/buffers/buffer_common.py
@@ -1,10 +1,12 @@
 import itertools
-buffer_sizes = [1, 5, 10, 20, 30, 40, 50, 75, 100]
-types = ['normal', 'resize'] #, 'no_circuit', 'strobe']
+# buffer_sizes = [1, 5, 10, 20, 30, 40, 50, 75, 100]
+buffer_sizes = [100]
+# types = ['normal', 'resize'] #, 'no_circuit', 'strobe']
+types = ['resize', 'strobe']
 CONFIGS = [{'type': t, 'buffer_size': b}
            for (t, b) in itertools.product(types, buffer_sizes)]
 
 for c in CONFIGS:
     if c['type'] == 'resize':
-        c['type'] = 'normal'
+        c['type'] = 'strobe'
         c['queue_resize'] = True
