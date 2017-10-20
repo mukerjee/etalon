ce3986124f90caad77d7e33d8e731a59ddd044c5
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-10-20 11:55:17 -0400

    can't use vars in for exp

--- cloudlab/arp_poison.sh
+++ cloudlab/arp_poison.sh
@@ -6,7 +6,7 @@ $HOME/sdrt/cloudlab/arp_clear.sh
 
 sudo ping switch -c1 -W1
 
-for i in {1..$NUM_HOSTS}
+for i in `seq 1 $NUM_HOSTS`
 do
     sudo arp -s host$i `arp | grep switch | tr -s ' ' | cut -d' ' -f3`
 done
