b1f697d64a7a9d0646df609b4ca24747b9784016
mukerjee <mukerjee@cs.cmu.edu>
2017-11-12 16:08:22 -0700

    reno

--- cloudlab/tune.sh
+++ cloudlab/tune.sh
@@ -27,6 +27,7 @@ sudo service irqbalance stop
 sudo /usr/sbin/set_irq_affinity.sh $CONTROL_IF
 
 sudo sysctl -w net.ipv4.neigh.default.gc_thresh3=2048
+sudo sysctl -w net.ipv4.tcp_congestion_control=reno
 
 for i in `seq 1 $NUM_RACKS`
 do
