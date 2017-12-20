c8cf5bcc4a29919aaa41513dc850375c54089cb5
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-12-20 16:52:14 -0500

    code cleanup + sending true ADU size to switch

--- adu-send/lib/adu-send.c
+++ adu-send/lib/adu-send.c
@@ -13,8 +13,8 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
-#define LOCAL_CTRL_DEVNAME "eth3"
-#define SWITCH_CTRL_IP "10.10.2.1"
+#define LOCAL_CTRL_DEVNAME "enp8s0d1"
+#define SWITCH_CTRL_IP "10.2.10.9"
 #define SWITCH_CTRL_PORT "8123"
 
 struct traffic_info {
@@ -32,7 +32,8 @@ int socket(int domain, int type, int protocol);
 int close(int sockfd);
 
 static int (*next_socket)(int domain, int type, int protocol) = NULL;
-static int (*next_send)(int socket, const void *buffer, size_t length, int flags) = NULL;
+static int (*next_send)(int socket, const void *buffer, size_t length,
+                        int flags) = NULL;
 static int (*next_write)(int fd, void *buffer, size_t size) = NULL;
 static int (*next_close)(int sockfd) = NULL;
 
@@ -43,7 +44,8 @@ void get_local_ip(int fd, char* saddr) {
   strncpy(ifr.ifr_name, LOCAL_CTRL_DEVNAME, IFNAMSIZ-1);
   ioctl(fd, SIOCGIFADDR, &ifr);
 
-  strncpy(saddr, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr), INET_ADDRSTRLEN);
+  strncpy(saddr, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr),
+          INET_ADDRSTRLEN);
 }
 
 void get_remote_ip(int fd, char* raddr) {
@@ -53,7 +55,6 @@ void get_remote_ip(int fd, char* raddr) {
   len = sizeof addr;
   getpeername(fd, (struct sockaddr*)&addr, &len);
 
-  // deal with both IPv4 and IPv6:
   if (addr.ss_family == AF_INET) {
     struct sockaddr_in *s = (struct sockaddr_in *)&addr;
     inet_ntop(AF_INET, &s->sin_addr, raddr, INET_ADDRSTRLEN);
@@ -62,9 +63,7 @@ void get_remote_ip(int fd, char* raddr) {
 
 static void open_ctrl_socket() {
   if(ctrl_sock == -1){
-    struct addrinfo hints, // Used to provide hints to getaddrinfo()
-                    *res,  // Used to return the list of addrinfo's
-                    *p;    // Used to iterate over this list
+    struct addrinfo hints, *res, *p;
 
     char *host=SWITCH_CTRL_IP, *port=SWITCH_CTRL_PORT;
 
@@ -78,15 +77,16 @@ static void open_ctrl_socket() {
     }
 
     for(p = res; p != NULL; p = p->ai_next)  {
-      if ((ctrl_sock = next_socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-	perror("Could not open socket");
-	continue;
+      if ((ctrl_sock = next_socket(p->ai_family, p->ai_socktype,
+                                   p->ai_protocol)) == -1) {
+        perror("Could not open socket");
+        continue;
       }
 
       if (connect(ctrl_sock, p->ai_addr, p->ai_addrlen) == -1)  {
-	next_close(ctrl_sock);
-	perror("Could not connect to socket");
-	continue;
+        next_close(ctrl_sock);
+        perror("Could not connect to socket");
+        continue;
       }
 
       break;
@@ -113,7 +113,7 @@ static void get_next_fn(void** next_fn, char* fn) {
 // Wrap up the socket() call
 int socket(int domain, int type, int protocol) {
   char* fn_name = "socket";
-  get_next_fn((void**)&next_socket,fn_name);
+  get_next_fn((void**)&next_socket, fn_name);
 
   open_ctrl_socket();
 
@@ -132,46 +132,50 @@ int socket(int domain, int type, int protocol) {
 ssize_t write(int fd, void *buffer, size_t size) {
   ssize_t nbytes = -1;
   char* fn_name = "write";
-  get_next_fn((void**)&next_write,fn_name);
+  get_next_fn((void**)&next_write, fn_name);
 
-  //Get the destination address
+  ssize_t true_size = next_write(fd, buffer, size);
+
+  // Get the destination address
   if (fd != ctrl_sock) {
     get_remote_ip(fd, info->dst);
-    info->size = size;
+    info->size = true_size;
     nbytes = next_write(ctrl_sock, info, sizeof(struct traffic_info));
 
     if (nbytes != sizeof(struct traffic_info)){
       fprintf(stderr, "Failed to send ctrl message\n");
       return nbytes;
     }
-  /* fprintf(stderr, "SIZE: %ld\n", size); */
+  /* fprintf(stderr, "SIZE: %ld\n", true_size); */
   }
-  return next_write(fd, buffer, size);
+  return true_size;
 }
 
 // Wrap up the send() call
 ssize_t send(int fd, const void *buffer, size_t size, int flags) {
   ssize_t nbytes = -1;
   char* fn_name = "send";
-  get_next_fn((void**)&next_send,fn_name);
+  get_next_fn((void**)&next_send, fn_name);
 
-  //Get the destination address
+  ssize_t true_size = next_send(fd, buffer, size, flags);
+
+  // Get the destination address
   if (fd != ctrl_sock) {
     get_remote_ip(fd, info->dst);
-    info->size = size;
+    info->size = true_size;
     nbytes = next_send(ctrl_sock, info, sizeof(struct traffic_info), flags);
     if (nbytes != sizeof(struct traffic_info)){
       fprintf(stderr, "Failed to send ctrl message\n");
       return nbytes;
     }
   }
-  return next_send(fd, buffer, size, flags);
+  return true_size;
 }
 
 // Wrap up the close() call
 int close(int sockfd) {
   char* fn_name = "close";
-  get_next_fn((void**)&next_close,fn_name);
+  get_next_fn((void**)&next_close, fn_name);
 
   /* fprintf(stderr, "close(%i)\n", sockfd); */
   /* next_close(ctrl_sock); */
