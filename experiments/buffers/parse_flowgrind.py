238cfe2c835886407a17792c895d50eb4864589f
mukerjee <mukerjee@cs.cmu.edu>
2017-12-25 09:41:19 -0700

    parse flowgrind

--- experiments/buffers/parse_flowgrind.py
+++ experiments/buffers/parse_flowgrind.py
@@ -2,6 +2,8 @@
 
 import sys
 import glob
+import os
+import tarfile
 
 from collections import defaultdict
 
@@ -12,10 +14,10 @@ def parse_flowgrind(fn):
     for l in open(fn):
         if 'seed' in l:
             l = l[1:].strip().split(',')
-            id = int(l[0].split(' ')[0])
-            rack = int(l[0].split(' ')[2].split('/')[0].split('.')[2])
+            id = int(l[0].split()[0])
+            rack = int(l[0].split()[2].split('/')[0].split('.')[2])
             flows[id].append(rack)
-            if l[0].split(' ')[1][0] == 'S':
+            if l[0].split()[1][0] == 'S':
                 time = float(l[7].split('=')[1].strip().split('/')[0][:-1])
                 tp = float(l[8].split('=')[1].strip().split('/')[0]) / 1000.0
             else:
@@ -42,6 +44,13 @@ def parse_flowgrind(fn):
 
 if __name__ == "__main__":
     fns = sys.argv[1]
+    if 'tar.gz' in fns:
+        dir = '/tmp/' + fns.split('.tar.gz')[0].split('/')[-1]
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
+        t = tarfile.open(fns)
+        t.extractall(dir)
+        fns = dir + '/*'
     # if 'tmp' not in fns:
     #     fns += '/tmp/*'
     for fn in glob.glob(fns):
