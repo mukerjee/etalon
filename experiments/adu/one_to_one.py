d5fa846ceb8400a746dc293e647a0a59082d969b
mukerjee <mukerjee@cs.cmu.edu>
2017-12-26 16:10:54 -0700

    initial adu experiments

--- experiments/adu/one_to_one.py
+++ experiments/adu/one_to_one.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import buffer_common
+import adu_common
 sys.path.append("../")
 
 from common import initializeExperiment, finishExperiment, flowgrind
@@ -9,7 +9,7 @@ from click_common import setConfig
 
 initializeExperiment(adu=True)
 
-for config in buffer_common.CONFIGS:
+for config in adu_common.CONFIGS:
     print '--- running test type %s...' % config['type']
     print '--- setting switch buffer size to %d...' % config['buffer_size']
     setConfig(config)
