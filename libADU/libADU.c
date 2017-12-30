c5d237a28eedab805ca1c11a8ac92f90035f3571
mukerjee <mukerjee@cs.cmu.edu>
2017-12-30 12:17:23 -0700

    accept wasn't being caught in libADU; realized i could greatly simplify the whole thing

--- libADU/libADU.c
+++ libADU/libADU.c
@@ -17,7 +17,6 @@
 #define SWITCH_CTRL_IP "10.2.10.9"
 #define SWITCH_CTRL_PORT "8123"
 
-#define MAX_FD 4096
 #define EXIT_FAILED -1
 
 struct traffic_info {
@@ -26,15 +25,18 @@ struct traffic_info {
   size_t size;
 };
 
-struct traffic_info* info_table[MAX_FD] = {NULL};
 char local_ip[NI_MAXHOST];
 int ctrl_sock = -1;
 
-static int (*next_socket)(int domain, int type, int protocol) = NULL;
-static int (*next_send)(int socket, const void *buffer, size_t length,
-                        int flags) = NULL;
-static int (*next_write)(int fd, void *buffer, size_t size) = NULL;
-static int (*next_close)(int sockfd) = NULL;
+// Find the library version of the function that we are wrapping
+static void get_next_fn(void** next_fn, char* fn) {
+  char* msg;
+  *next_fn = dlsym(RTLD_NEXT, fn);
+  if ((msg = dlerror()) != NULL) {
+    fprintf(stderr, "dlopen failed on %s: %s\n", fn, msg);
+    exit(EXIT_FAILED);
+  }
+}
 
 void get_local_ip(int fd, char* saddr) {
   struct sockaddr_in localAddress;
@@ -73,6 +75,8 @@ static void open_ctrl_socket() {
     }
 
     for(p = res; p != NULL; p = p->ai_next)  {
+      int (*next_socket)(int, int, int);
+      get_next_fn((void**)&next_socket, "socket");
       if ((ctrl_sock = next_socket(p->ai_family, p->ai_socktype,
                                    p->ai_protocol)) == -1) {
         perror("Could not open socket");
@@ -80,6 +84,8 @@ static void open_ctrl_socket() {
       }
 
       if (connect(ctrl_sock, p->ai_addr, p->ai_addrlen) == -1)  {
+	int (*next_close)(int);
+	get_next_fn((void**)&next_close, "close");
         next_close(ctrl_sock);
         perror("Could not connect to socket");
         continue;
@@ -91,113 +97,47 @@ static void open_ctrl_socket() {
   }
 }
 
-// Find the library version of the function that we are wrapping
-static void get_next_fn(void** next_fn, char* fn) {
-  char* msg;
-
-  if(! *next_fn){
-    *next_fn = dlsym(RTLD_NEXT, fn);
-    if ((msg = dlerror()) != NULL) {
-      fprintf(stderr, "dlopen failed on %s: %s\n", fn, msg);
-      exit(1);
-    } else {
-      /* fprintf(stderr,  "next_%s = %p\n", fn, *next_fn); */
-    }
-  }
-}
-
-// Wrap up the socket() call
-int socket(int domain, int type, int protocol) {
-  char* fn_name = "socket";
-  get_next_fn((void**)&next_socket, fn_name);
-  get_next_fn((void**)&next_close, fn_name);
+void send_adu_info(int fd, int true_size) {
+  if (fd == ctrl_sock)
+    return;
 
   open_ctrl_socket();
 
-  int sockfd = next_socket(domain, type, protocol);
+  struct traffic_info adu_info;
+  get_local_ip(fd, adu_info.src);
+  get_remote_ip(fd, adu_info.dst);
+  adu_info.size = true_size;
+  // fprintf(stderr, "LOCAL_IP: %s REMOTE_IP: %s\n", adu_info.src, traffic_info.dst);
 
-  if (sockfd > MAX_FD) {
-    fprintf(stderr, "Sockfd larger than MAX_FD\n");
-    return -1;
+  ssize_t (*next_write)(int, void*, size_t);
+  get_next_fn((void**)&next_write, "write");
+  ssize_t nbytes = next_write(ctrl_sock, &adu_info, sizeof(struct traffic_info));
+  if (nbytes != sizeof(struct traffic_info)){
+    fprintf(stderr, "Failed to send ctrl message\n");
+    exit(EXIT_FAILED);
   }
-  if (sockfd < 0) {
-    return sockfd;
-  }
-
-  info_table[sockfd] = (struct traffic_info *)malloc(sizeof(struct traffic_info));
-  memset(info_table[sockfd], 0, sizeof(struct traffic_info));
-
-  return sockfd;
 }
 
 // Wrap up the write() call
 ssize_t write(int fd, void *buffer, size_t size) {
-  ssize_t nbytes = -1;
-
-  if (!next_write) {
-    char* fn_name = "write";
-    get_next_fn((void**)&next_write, fn_name);
-  }
+  ssize_t (*next_write)(int, void*, size_t);
+  get_next_fn((void**)&next_write, "write");
 
   ssize_t true_size = next_write(fd, buffer, size);
 
-  // Get the destination address
-  if (info_table[fd] && fd != ctrl_sock) {
-    get_local_ip(fd, info_table[fd]->src);
-    get_remote_ip(fd, info_table[fd]->dst);
-    /* fprintf(stderr, "LOCAL_IP: %s REMOTE_IP: %s\n", info_table[fd]->src, */
-    /* 	    info_table[fd]->dst); */
-    info_table[fd]->size = true_size;
-    nbytes = next_write(ctrl_sock, info_table[fd], sizeof(struct traffic_info));
+  send_adu_info(fd, true_size);
 
-    if (nbytes != sizeof(struct traffic_info)){
-      fprintf(stderr, "Failed to send ctrl message\n");
-      exit(EXIT_FAILED);
-    }
-  /* fprintf(stderr, "SIZE: %ld\n", true_size); */
-  }
   return true_size;
 }
 
 // Wrap up the send() call
 ssize_t send(int fd, const void *buffer, size_t size, int flags) {
-  ssize_t nbytes = -1;
-
-  if (!next_write) {
-    char* fn_name = "write";
-    get_next_fn((void**)&next_write, fn_name);
-  }
-
-  char* fn_name = "send";
-  get_next_fn((void**)&next_send, fn_name);
+  ssize_t (*next_send)(int, const void*, size_t, int);
+  get_next_fn((void**)&next_send, "send");
 
   ssize_t true_size = next_send(fd, buffer, size, flags);
 
-  // Get the destination address
-  if (info_table[fd] && fd != ctrl_sock) {
-    get_local_ip(fd, info_table[fd]->src);
-    get_remote_ip(fd, info_table[fd]->dst);
-    /* fprintf(stderr, "LOCAL_IP: %s REMOTE_IP: %s\n", info_table[fd]->src, */
-    /* 	    info_table[fd]->dst); */
-    info_table[fd]->size = true_size;
-    nbytes = next_write(ctrl_sock, info_table[fd], sizeof(struct traffic_info));
-    if (nbytes != sizeof(struct traffic_info)){
-      fprintf(stderr, "Failed to send ctrl message\n");
-      exit(EXIT_FAILED);
-    }
-  }
+  send_adu_info(fd, true_size);
+
   return true_size;
-}
-
-// Wrap up the close() call
-int close(int sockfd) {
-  char* fn_name = "close";
-  get_next_fn((void**)&next_close, fn_name);
-
-  free(info_table[sockfd]);
-  info_table[sockfd] = NULL;
-
-  /* fprintf(stderr, "close(%i)\n", sockfd); */
-  /* next_close(ctrl_sock); */
-  return next_close(sockfd);
 }
