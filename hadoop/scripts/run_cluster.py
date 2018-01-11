58d802adb488265539591bab798a87ff602f6207
Daehyeok Kim <dkimcs@gmail.com>
2018-01-11 09:26:50 -0700

    Update hadoop scripts

--- hadoop/scripts/run_cluster.py
+++ hadoop/scripts/run_cluster.py
@@ -6,11 +6,12 @@ import argparse
 import os
 import re
 
-USERNAME='mukerjee'
+VERSION='3.0.0-SNAPSHOT'
+USERNAME = 'mukerjee'
 
-publicIP = {'namenode':['10.10.1.11'], 'datanode':['10.10.1.12', '10.10.1.21', '10.10.1.22']}
-privateDNS = {'namenode':['10.10.1.11'], 'datanode':['10.10.1.12', '10.10.1.21', '10.10.1.22']}
-namenode_publicDNS = '10.10.1.11'
+publicIP = {'namenode':['10.1.10.1'], 'datanode':['10.1.10.2', '10.1.10.3', '10.1.10.4']}
+privateDNS = {'namenode':['10.1.10.1'], 'datanode':['10.1.10.2', '10.1.10.3', '10.1.10.4']}
+namenode_publicDNS = '10.1.10.1'
 num_datanode = 3
 
 def run_cmd(host, cmd):
@@ -29,10 +30,10 @@ def run_cmd(host, cmd):
 
 def run_cluster():
 
-    HADOOP_CONF_DIR = '/hadoop/etc/hadoop'
-    HADOOP_INSTALL = '/hadoop'
-    HADOOP_BIN = '/hadoop/bin'
-    HADOOP_SBIN = '/hadoop/sbin'
+    HADOOP_INSTALL = '/usr/local/hadoop'
+    HADOOP_CONF_DIR = '/usr/local/hadoop/etc/hadoop'
+    HADOOP_BIN = '/usr/local/hadoop/bin'
+    HADOOP_SBIN = '/usr/local/hadoop/sbin'
 
     #Install hadoop
     run_cmd(publicIP['namenode'][0], '%s/start-dfs.sh'%HADOOP_SBIN)
@@ -40,13 +41,13 @@ def run_cluster():
     run_cmd(publicIP['namenode'][0], '%s/mr-jobhistory-daemon.sh start historyserver'%HADOOP_SBIN)
 
     # Set datanode
-    for i in range (0, num_datanode):
-        addr = publicIP['datanode'][i].split('.')[3]
-        if (addr == '12' or addr[1] == '1'):
-            #it's already running
-            continue
-        print 're-start datanode %d'%i
-        run_cmd(publicIP['datanode'][i], '%s/run-additionalDN.sh start 1' % (HADOOP_INSTALL))
+    #for i in range (0, num_datanode):
+    #    addr = publicIP['datanode'][i].split('.')[3]
+    #    if (addr == '12' or addr[1] == '1'):
+    #        #it's already running
+    #        continue
+    #    print 're-start datanode %d'%i
+    #    run_cmd(publicIP['datanode'][i], '%s/run-additionalDN.sh start 1' % (HADOOP_INSTALL))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
