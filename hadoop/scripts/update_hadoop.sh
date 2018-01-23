8b1701a73aa2fa00085495044b3c05db98fb7bf0
mukerjee <mukerjee@cs.cmu.edu>
2018-01-23 16:40:58 -0700

    push hadoop update script

---
+++ hadoop/scripts/update_hadoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUM_RACKS=8
+
+for i in `seq 1 $NUM_RACKS`
+do
+    echo $i
+    scp -o StrictHostKeyChecking=no ~/hadoop-2.7.5.tar.gz host$i:/sdrt/vhost/
+    scp -o StrictHostKeyChecking=no ~/hadoop-SDRT-2.7.5.tar.gz host$i:/sdrt/vhost/
+    scp -o StrictHostKeyChecking=no ~/HiBench.tar.gz host$i:/sdrt/vhost/
+done
