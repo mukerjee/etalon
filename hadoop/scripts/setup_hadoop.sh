39766a07fd29f7e874688c656b94c5bf568f830e
mukerjee <mukerjee@cs.cmu.edu>
2018-01-24 12:42:06 -0700

    moving hdfs mount point

--- hadoop/scripts/setup_hadoop.sh
+++ hadoop/scripts/setup_hadoop.sh
@@ -4,6 +4,8 @@ cd /sdrt/vhost/
 tar xfvz hadoop-2.7.5.tar.gz
 sudo mv hadoop-2.7.5 /usr/local/hadoop
 cp /sdrt/hadoop/config/* /usr/local/hadoop/etc/hadoop/
-sudo mkdir -p /usr/local/hadoop/hadoop_data/hdfs/namenode
-sudo mkdir -p /usr/local/hadoop/hadoop_data/hdfs/datanode
+sudo mkdir -p /mnt/hdfs
+/usr/local/etc/emulab/mkextrafs.pl /mnt/hdfs
+sudo mkdir -p /mnt/hdfs/namenode
+sudo mkdir -p /mnt/hdfs/datanode
 
