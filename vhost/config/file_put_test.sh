548b84c97df01383f2eb53e08f0d50e985c6671d
mukerjee <mukerjee@cs.cmu.edu>
2018-01-24 09:57:28 -0700

    fpt should write to different files

--- vhost/config/file_put_test.sh
+++ vhost/config/file_put_test.sh
@@ -2,10 +2,11 @@
 
 rm /root/fpt_results.txt &> /dev/null
 touch /root/fpt_results.txt
+dir=hostname | cut -d'.' -f1
 dd if=/dev/zero of=/root/temp_data.dat bs=128M count=1
-/usr/local/hadoop/bin/hadoop fs -mkdir /test
+/usr/local/hadoop/bin/hadoop fs -mkdir /test-$dir
 sleep $((60 - $(date +%s) % 60))
 for i in {1..1}
 do
-    { time /usr/local/hadoop/bin/hadoop fs -put -f /root/temp_data.dat /test/$i.dat ; } >> /root/fpt_results.txt 2>&1
+    { time /usr/local/hadoop/bin/hadoop fs -put -f /root/temp_data.dat /test-$dir/$i.dat ; } >> /root/fpt_results.txt 2>&1
 done
