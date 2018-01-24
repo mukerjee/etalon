f48935c054feb0bc63318ecb6cc952341ec5a573
mukerjee <mukerjee@cs.cmu.edu>
2018-01-24 09:29:39 -0700

    fpt

--- vhost/config/file_put_test.sh
+++ vhost/config/file_put_test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+rm /root/fpt_results.txt &> /dev/null
+touch /root/fpt_results.txt
 dd if=/dev/zero of=/root/temp_data.dat bs=128M count=1
 /usr/local/hadoop/bin/hadoop fs -mkdir /test
+sleep $((60 - $(date +%s) % 60))
 for i in {1..1}
 do
-    time /usr/local/hadoop/bin/hadoop fs -put -f /root/temp_data.dat /test/$i.dat
+    { time /usr/local/hadoop/bin/hadoop fs -put -f /root/temp_data.dat /test/$i.dat ; } >> /root/fpt_results.txt 2>&1
 done
