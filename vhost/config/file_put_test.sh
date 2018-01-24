370da1de99df56778742e9f1fb8e487e9334a918
mukerjee <mukerjee@cs.cmu.edu>
2018-01-24 10:05:51 -0700

    typo

--- vhost/config/file_put_test.sh
+++ vhost/config/file_put_test.sh
@@ -2,7 +2,7 @@
 
 rm /root/fpt_results.txt &> /dev/null
 touch /root/fpt_results.txt
-dir=hostname | cut -d'.' -f1
+dir=`hostname | cut -d'.' -f1`
 dd if=/dev/zero of=/root/temp_data.dat bs=128M count=1
 /usr/local/hadoop/bin/hadoop fs -mkdir /test-$dir
 sleep $((60 - $(date +%s) % 60))
