d81b0fbb1472c97aba54117c2d179cfd22518223
mukerjee <mukerjee@cs.cmu.edu>
2018-01-24 11:44:43 -0700

    physical version of fpt

---
+++ hadoop/scripts/file_put_physical_hosts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm $HOME/fpt_results.txt &> /dev/null
+touch $HOME/fpt_results.txt
+dir=`hostname | cut -d'.' -f1`
+dd if=/dev/zero of=$HOME/temp_data.dat bs=128M count=1
+/usr/local/hadoop/bin/hadoop fs -mkdir /test-$dir
+sleep $((60 - $(date +%s) % 60))
+for i in {1..1}
+do
+    { time /usr/local/hadoop/bin/hadoop fs -put -f $HOME/temp_data.dat /test-$dir/$i.dat ; } >> /root/fpt_results.txt 2>&1
+done
