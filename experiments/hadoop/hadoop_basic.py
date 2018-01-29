d94dd8a5412d9ae4e7433794b8de0fc21b5c69bb
mukerjee <mukerjee@cs.cmu.edu>
2018-01-29 00:22:05 -0700

    hadoop experiment runner

--- experiments/hadoop/hadoop_basic.py
+++ experiments/hadoop/hadoop_basic.py
@@ -5,25 +5,35 @@ sys.path.append("../")
 
 import os
 
-from common import initializeExperiment, finishExperiment, flowgrind
+from common import initializeExperiment, finishExperiment, dfsioe
 from click_common import setConfig
 
-initializeExperiment(hadoop=True)
-nn = 'h11.sdrt.cs.cmu.edu'
-cmd = 'scp -o StrictHostKeyChecking=no ~/HiBench.tar.gz root@%s:/root/' % nn
-os.system(cmd)
-cmd = 'ssh -o StrictHostKeyChecking=no root@%s ' \
-      '"tar xfvz HiBench.tar.gz &> /dev/null && rm HiBench.tar.gz"' % nn
-dnull = os.system(cmd)
+CONFIGS = [
+    {'type': 'normal', 'traffic_source': 'QUEUE'},
+    {'type': 'resize', 'traffic_source': 'QUEUE', 'in_advance': 20000},
+    {'type': 'resize', 'traffic_source': 'QUEUE', 'in_advance': 20000, 'cc': 'ocs'},
+    {'type': 'normal', 'traffic_source': 'ADU'},
+    {'type': 'resize', 'traffic_source': 'ADU', 'in_advance': 20000},
+    {'type': 'resize', 'traffic_source': 'ADU', 'in_advance': 20000, 'cc': 'ocs'},
+]
 
-#for config in buffer_common.CONFIGS:
-#    print '--- running test type %s...' % config['type']
-#    print '--- setting switch buffer size to %d...' % config['buffer_size']
-#    setConfig(config)
-#    print '--- done...'
-#
-#    settings = {'flows': []}
-#    settings['flows'].append({'src': 'r1', 'dst': 'r2'})
-#    flowgrind(settings)
+for c in CONFIGS:
+    c['packet_log'] = False
+    if c['type'] == 'resize':
+        c['type'] = 'normal'
+        c['queue_resize'] = True
+    else:
+        c['queue_resize'] = False
+    if 'cc' not in c:
+        c['cc'] = 'reno'
 
-#finishExperiment()
+for h in ['Hadoop', 'Hadoop-SDRT']:
+    for c in CONFIGS:
+        c['hadoop'] = h
+        print '--- running %s, %s, %s, %s' % (h, c['traffic_source'], c['queue_resize'], c['cc'])
+        initializeExperiment(adu=(c['traffic_source']=='ADU'), hadoop=c['hadoop'])
+        setConfig(c)
+        print '--- done initializing...'
+        dfsioe('h21', h)
+
+finishExperiment()
