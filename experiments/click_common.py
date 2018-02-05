0e339cefe6022c5b892ebb7a5728dec630b499bd
mukerjee <mukerjee@cs.cmu.edu>
2018-02-05 12:53:53 -0700

    more cleanup

--- experiments/click_common.py
+++ experiments/click_common.py
@@ -1,18 +1,13 @@
 import socket
 import time
 import threading
-from globals import NUM_RACKS, TIMESTAMP, SCRIPT, TDF, EXPERIMENTS, \
-    PHYSICAL_NODES, RPYC_CONNECTIONS
+from python_config import NUM_RACKS, TIMESTAMP, SCRIPT, TDF, EXPERIMENTS, \
+    PHYSICAL_NODES, RPYC_CONNECTIONS, CLICK_ADDR, CLICK_PORT, CLICK_BUFFER_SIZE
 import common
 
-TCP_IP = 'localhost'
-TCP_PORT = 1239
-BUFFER_SIZE = 1024
 CLICK_SOCKET = None
-
 CURRENT_CONFIG = {}
 FN_FORMAT = ''
-
 CURRENT_CC = ''
 
 
@@ -23,8 +18,8 @@ def initializeClickControl():
     global CLICK_SOCKET
     print '--- connecting to click socket...'
     CLICK_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    CLICK_SOCKET.connect((TCP_IP, TCP_PORT))
-    CLICK_SOCKET.recv(BUFFER_SIZE)
+    CLICK_SOCKET.connect((CLICK_ADDR, CLICK_PORT))
+    CLICK_SOCKET.recv(CLICK_BUFFER_SIZE)
     print '--- done...'
 
 
@@ -32,14 +27,14 @@ def clickWriteHandler(element, handler, value):
     message = "WRITE %s.%s %s\n" % (element, handler, value)
     print message.strip()
     CLICK_SOCKET.send(message)
-    print CLICK_SOCKET.recv(BUFFER_SIZE).strip()
+    print CLICK_SOCKET.recv(CLICK_BUFFER_SIZE).strip()
 
 
 def clickReadHandler(element, handler):
     message = "READ %s.%s\n" % (element, handler)
     print message.strip()
     CLICK_SOCKET.send(message)
-    data = CLICK_SOCKET.recv(BUFFER_SIZE).strip()
+    data = CLICK_SOCKET.recv(CLICK_BUFFER_SIZE).strip()
     print data
     return data
 
