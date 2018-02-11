7f774d5c9a11d8a4402e5ea978dba37595f699a0
mukerjee <mukerjee@cs.cmu.edu>
2018-02-11 08:31:16 -0700

    need ns run to do arp in container

--- rpycd/rpycd.py
+++ rpycd/rpycd.py
@@ -9,6 +9,8 @@ sys.path.insert(0, '/etalon/etc')
 from python_config import RPYC_PORT, SWITCH_CONTROL_IP
 
 DOCKER_EXEC = 'sudo docker exec -t h{id} {cmd}'
+DOCKER_GET_PID = "sudo docker inspect --format '{{{{.State.Pid}}}}' h{id}"
+NS_RUN = 'sudo nsenter -t {pid} -n {cmd}'
 SELF_ID = int(socket.gethostname().split('.')[0][-1])
 
 
@@ -30,6 +32,10 @@ class EtalonService(rpyc.Service):
 
     def exposed_run_host(self, my_id, my_cmd):
         return self.call(DOCKER_EXEC.format(id=my_id, cmd=my_cmd))
+
+    def exposed_ns_run(self, my_id, my_cmd):
+        my_pid = self.call(DOCKER_GET_PID.format(id=my_id)).split()[0].strip()
+        return self.call(NS_RUN.format(pid=my_pid, cmd=my_cmd))
 
     def exposed_run(self, my_cmd):
         return self.call(cmd=my_cmd)
