35bf3e78a97ce231657dd9a44130679ab41e1e66
Matt Mukerjee <mukerjee@cs.cmu.edu>
2018-02-27 20:26:09 -0500

    arp poison typos

--- bin/arp_poison.sh
+++ bin/arp_poison.sh
@@ -2,11 +2,11 @@
 
 source /etalon/etc/script_config.sh
 
-etalon/bin/arp_clear.sh
+/etalon/bin/arp_clear.sh
 
 sudo ping switch -c1 -W1
 
-for i in `seq 1 $NUM_HOSTS`
+for i in `seq 1 $NUM_RACKS`
 do
     sudo arp -s host$i `arp | grep switch | tr -s ' ' | cut -d' ' -f3`
 done
