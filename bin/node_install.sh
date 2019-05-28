519d49151d240f983c3104be6fe4da9d8cd42601
Christopher Canel <c.canel@icloud.com>
2019-05-28 20:30:09 +0000

    Format fixes and remove unused code in node_install.sh

--- bin/node_install.sh
+++ bin/node_install.sh
@@ -28,16 +28,15 @@ sudo ln -sf ~/etalon &&
 (crontab -l 2>/dev/null; echo "@reboot sleep 60 && $HOME/etalon/bin/tune.sh") | crontab - &&
 sudo rm -f /var/run/crond.reboot &&
 
-
-# Mellanox OFED
+# Mellanox OFED.
 # https://docs.mellanox.com/display/MLNXOFEDv461000/Introduction
 cd &&
-# Set up repo
+# Set up repo.
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-$OFED_VERSION/MLNX_OFED_LINUX-$OFED_VERSION-ubuntu$UBUNTU_VERSION-x86_64.iso &&
 sudo mount -o ro,loop MLNX_OFED_LINUX-$OFED_VERSION-ubuntu$UBUNTU_VERSION-x86_64.iso /mnt &&
 echo "deb file:/mnt/DEBS /" | sudo tee /etc/apt/sources.list.d/mlnx_ofed.list &&
 wget -qO - http://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox | sudo apt-key add - &&
-# Install
+# Install.
 sudo apt update &&
 sudo apt install -y mlnx-ofed-all mlnx-ofed-dpdk &&
 sudo connectx_port_config -c eth,eth &&
@@ -46,19 +45,19 @@ sudo /etc/init.d/openibd restart &&
 sudo rm -fv /etc/apt/sources.list.d/mlnx_ofed.list &&
 sudo umount /mnt &&
 
-# get docker
+# Docker.
 cd $HOME &&
 curl -fsSL https://get.docker.com -o get-docker.sh &&
 sudo sh get-docker.sh &&
 
-# get pipework
+# Pipework.
 cd $HOME &&
 sudo bash -c "curl https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework > /usr/local/bin/pipework" &&
 sudo chmod +x /usr/local/bin/pipework &&
 
 sudo systemctl enable /etalon/rpycd/rpycd.service &&
 
-# PTP
+# PTP.
 printf 'slaveOnly\t\t1\n[enp68s0]\n' | sudo tee -a /etc/linuxptp/ptp4l.conf &&
 sudo sed -i '/(PTP) service/a Requires=network.target\nAfter=network.target' /lib/systemd/system/ptp4l.service &&
 sudo sed -i 's/ -i eth0//' /lib/systemd/system/ptp4l.service &&
@@ -68,12 +67,7 @@ sudo systemctl enable phc2sys.service &&
 sudo systemctl disable ntp.service &&
 
 if [ $MODE != "--local" ]; then
-    # get extra space
-    sudo mkdir /mnt/hdfs &&
-    sudo /usr/local/etc/emulab/mkextrafs.pl -f /mnt/hdfs &&
-    sudo chown `whoami` /mnt/hdfs &&
-
-    # move docker dir
+    # Move docker dir.
     sudo service docker stop &&
     sudo mv /var/lib/docker /mnt/hdfs/ &&
     sudo ln -s /mnt/hdfs/docker /var/lib/docker &&
