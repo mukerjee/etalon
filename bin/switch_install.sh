437bdece99be9d18a6e4184c4f6ac97768fcb9f4
Christopher Canel <c.canel@icloud.com>
2019-05-28 19:45:05 +0000

    Formatting and comments

--- bin/switch_install.sh
+++ bin/switch_install.sh
@@ -50,22 +50,22 @@ git submodule update --init
 (crontab -l 2>/dev/null; echo "@reboot sleep 60 && /etalon/bin/tune.sh") | crontab - &&
 sudo rm -f /var/run/crond.reboot &&
 
-# Mellanox OFED
+# Mellanox OFED.
 # https://docs.mellanox.com/display/MLNXOFEDv461000/Introduction
 cd &&
-# Set up repo
+# Set up repo.
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-$OFED_VERSION/MLNX_OFED_LINUX-$OFED_VERSION-ubuntu$UBUNTU_VERSION-x86_64.iso &&
 sudo mount -o ro,loop MLNX_OFED_LINUX-$OFED_VERSION-ubuntu$UBUNTU_VERSION-x86_64.iso /mnt &&
 echo "deb file:/mnt/DEBS /" | sudo tee /etc/apt/sources.list.d/mlnx_ofed.list &&
 wget -qO - http://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox | sudo apt-key add - &&
-# Install
+# Install.
 sudo apt update &&
 sudo apt install -y mlnx-ofed-all mlnx-ofed-dpdk &&
 # Clean up, but keep the ISO as a record.
 sudo rm -fv /etc/apt/sources.list.d/mlnx_ofed.list &&
 sudo umount /mnt &&
 
-# Mellanox DPDK
+# Mellanox DPDK.
 # http://www.mellanox.com/related-docs/prod_software/MLNX_DPDK_Quick_Start_Guide_v16.11_2.3.pdf
 echo "Installing Mellanox DPDK..." &&
 cd &&
@@ -78,8 +78,7 @@ rm -fv dpdk-$DPDK_VERSION.tar.xz &&  # Keep the untarred files as a record.
 cd ./dpdk-stable-$DPDK_VERSION &&
 make -j `nproc` install T=x86_64-native-linuxapp-gcc &&
 
-# Huge pages
-# http://dpdk.org/doc/guides/linux_gsg/sys_reqs.html
+# Huge pages. http://dpdk.org/doc/guides/linux_gsg/sys_reqs.html
 echo "Setting up huge pages..." &&
 # Configure huge pages to be allocated on boot.
 sudo sed -i -r 's/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 default_hugepagesz=1G hugepagesz=1G hugepages=4\"/' /etc/default/grub &&
@@ -88,7 +87,7 @@ sudo rm -rf /mnt/huge_1GB &&
 sudo mkdir /mnt/huge_1GB &&
 echo 'nodev /mnt/huge_1GB hugetlbfs pagesize=1GB 0 0' | sudo tee -a /etc/fstab &&
 
-# RTE_SDK location
+# RTE_SDK location.
 echo "Setting RTE_SDK location..." &&
 echo "" >> $HOME/.bashrc &&
 echo "export RTE_SDK=$HOME/dpdk-stable-$DPDK_VERSION" >> $HOME/.bashrc &&
@@ -96,14 +95,14 @@ echo "export RTE_TARGET=x86_64-native-linuxapp-gcc" >> $HOME/.bashrc &&
 export RTE_SDK=$HOME/dpdk-stable-$DPDK_VERSION &&
 export RTE_TARGET=x86_64-native-linuxapp-gcc &&
 
-# make Click
+# Click.
 echo "Installing Click..." &&
 cd /etalon/click-etalon &&
 ./configure --enable-user-multithread --disable-linuxmodule --enable-intel-cpu --enable-nanotimestamp --enable-dpdk &&
 make -j `nproc` &&
 sudo make -j `nproc` install &&
 
-# make flowgrind
+# Flowgrind.
 echo "Installing flowgrind..." &&
 cd /etalon/flowgrind-etalon &&
 autoreconf -i &&
@@ -112,7 +111,7 @@ make -j `nproc` &&
 sudo make -j `nproc` install &&
 cp /usr/local/sbin/flowgrindd /etalon/vhost/ &&
 
-# libVT
+# libVT.
 echo "Installing libVT..." &&
 cd /etalon/libVT &&
 sudo make -j `nproc` install &&
@@ -123,14 +122,14 @@ sudo cp ./libVT.so /etalon/vhost/ &&
 # cd /etalon/libADU &&
 # sudo make -j `nproc` install &&
 
-# get docker
+# Docker.
 echo "Installing docker..." &&
 cd &&
 curl -fsSL https://get.docker.com -o get-docker.sh &&
 sudo sh get-docker.sh &&
 rm $HOME/get-docker.sh &&
 
-# PTP
+# PTP.
 echo "Setting up PTP..." &&
 printf '[enp68s0]\n' | sudo tee -a /etc/linuxptp/ptp4l.conf &&
 sudo sed -i '/(PTP) service/a Requires=network.target\nAfter=network.target' /lib/systemd/system/ptp4l.service &&
@@ -140,7 +139,7 @@ sudo systemctl daemon-reload &&
 sudo systemctl enable phc2sys.service &&
 sudo systemctl disable ntp.service &&
 
-# vhost SSH
+# vhost SSH.
 echo "Setting up SSH..." &&
 cp /etalon/vhost/config/ssh/id_rsa $HOME/.ssh/ &&
 cp /etalon/vhost/config/ssh/id_rsa.pub $HOME/.ssh/ &&
@@ -196,14 +195,14 @@ chmod 600 $HOME/.ssh/id_rsa.pub &&
 # sudo rm -rf $HOME/procps* &&
 
 if [ $MODE != "--local" ]; then
-    # get extra space - Ignoring for now.
+    # Get extra space - Ignoring for now.
     # echo "Getting extra space..." &&
     # cd &&
     # sudo mkdir /mnt/hdfs &&
     # sudo /usr/local/etc/emulab/mkextrafs.pl -f /mnt/hdfs &&
     # sudo chown `whoami` /mnt/hdfs &&
 
-    # move docker dir
+    # Move docker dir.
     echo "Moving docker dir..." &&
     sudo service docker stop &&
     sudo mv /var/lib/docker /mnt/hdfs/ &&
