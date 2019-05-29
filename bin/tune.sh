fde671ca4196dda177c7a5490a564647f9080bb3
Christopher Canel <c.canel@icloud.com>
2019-05-28 23:14:34 -0400

    Clean up tune.sh

--- bin/tune.sh
+++ bin/tune.sh
@@ -3,8 +3,7 @@
 source /etalon/etc/script_config.sh
 
 # set IPs
-if ! hostname | grep -q switch
-then
+if ! hostname | grep -q switch; then
     h=`hostname | cut -d'.' -f1`
 else
     h='host100'
@@ -30,27 +29,24 @@ sudo sysctl -w net.ipv4.tcp_congestion_control=reno
 printf "%s\t%s\n" "127.0.0.1" "localhost" | sudo tee /etc/hosts
 printf "%s\t%s\n" $SWITCH_DATA_IP "switch" | sudo tee -a /etc/hosts
 
-for i in `seq 1 $NUM_RACKS`
-do
+for i in `seq 1 $NUM_RACKS`; do
     printf "%s\t%s\n" "10.$DATA_NET.100.$i" "host$i" | sudo tee -a /etc/hosts
 done
 
-for i in `seq 1 $NUM_RACKS`
-do
-    for j in `seq 1 $HOSTS_PER_RACK`
-    do
+for i in `seq 1 $NUM_RACKS`; do
+    for j in `seq 1 $HOSTS_PER_RACK`; do
         printf "%s\t%s\n" "10.$DATA_NET.$i.$j" "h$i$j.$FQDN" | sudo tee -a /etc/hosts
     done
 done
 
-if hostname | grep -q switch
-then  # switch
+if hostname | grep -q switch; then
+    # switch
     sudo sed -i "s/10\.$DATA_NET\./10\.$CONTROL_NET\./" /etc/hosts
 fi
 
 # build reTCP
 cd /etalon/reTCP/
-make
+make -j `nproc`
 sudo insmod retcp.ko
 
 ulimit -n 4096
