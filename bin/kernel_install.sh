b477549f9f26ee72b530af7d03c74cd3abb5b934
Christopher Canel <c.canel@icloud.com>
2019-05-30 10:20:52 -0400

    Handle errors in the kernel compile

--- bin/kernel_install.sh
+++ bin/kernel_install.sh
@@ -40,10 +40,15 @@ git clone git://kernel.ubuntu.com/ubuntu/ubuntu-$UBUNTU_VERSION.git $BUILD_DIR/u
 cd $BUILD_DIR/ubuntu-$UBUNTU_VERSION
 git apply $BUILD_DIR/etalon/reTCP/kernel-patch.patch
 fakeroot debian/rules clean
-# For some unknown reason, this command fails with a return code of 2 but does
-# not print any error messages. Strangley, running it again solves the problem.
-MAKEFLAGS="-j `nproc`" fakeroot debian/rules binary-headers binary-generic binary-perarch || \
-    MAKEFLAGS="-j `nproc`" fakeroot debian/rules binary-headers binary-generic binary-perarch
+# For some unknown reason, the compile command fails with an exit code of 2 but
+# does not print any error messages. Running it again solves the problem, but
+# also returns an erroneous exit code. Bash order of operations ensures that, if
+# this is line n, then if line n + 1 fails, then line n + 2 will be executed.
+# Line n + 3 ensures that lines n + 1 and n + 2 always return an exit code of 0.
+# In other words, the program will only stop if there is an error on line n + 4.
+MAKEFLAGS="-j `nproc`" fakeroot debian/rules binary-headers binary-generic binary-perarch ||
+MAKEFLAGS="-j `nproc`" fakeroot debian/rules binary-headers binary-generic binary-perarch ||
+true
 sudo dpkg -i $BUILD_DIR/*.deb
 
 # Clean up.
