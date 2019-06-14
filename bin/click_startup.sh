02a26e630acbc3c99cee7d3ed8589628365d8303
Christopher Canel <c.canel@icloud.com>
2019-06-14 16:25:27 +0000

    Delete allocated huge pages and set coremask in click_startup.sh

--- bin/click_startup.sh
+++ bin/click_startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# run click
-# n = 4? n = 3?
+/etalon/bin/gen-switch.py > /tmp/hybrid-switch.click
+# Clear allocated hugepages, in case the previous run crashed and did not
+# deallocate them. Not sure if this is actually necessary, but just in case...
+sudo rm -rfv /dev/hugepages/* /mnt/huge/*
+# Run the hybrid router.
+# Memory channels... n = 4? n = 3?
 # http://dpdk-guide.gitlab.io/dpdk-guide/tuning/memory.html
 # http://dpdk.org/doc/guides/nics/mlx4.html
-/etalon/bin/gen-switch.py > /tmp/hybrid-switch.click
-sudo nice -n -20 click --dpdk -c 0xFFFF -n 4 --  /tmp/hybrid-switch.click
+sudo nice -n -20 click --dpdk -l 0-39 -n 4 --  /tmp/hybrid-switch.click
