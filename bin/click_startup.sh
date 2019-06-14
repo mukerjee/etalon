82ede85c661c50a10bde0485e16ff9055135a6d7
Christopher Canel <c.canel@icloud.com>
2019-06-14 20:04:45 +0000

    Use only odd-numbered cores (socket 1) for Click

--- bin/click_startup.sh
+++ bin/click_startup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Generate hybrid switch definition.
 /etalon/bin/gen-switch.py > /tmp/hybrid-switch.click
 # Clear allocated hugepages, in case the previous run crashed and did not
-# deallocate them. Not sure if this is actually necessary, but just in case...
+# deallocate them. (Not sure if this is actually necessary.)
 sudo rm -rfv /dev/hugepages/* /mnt/huge/*
-# Run the hybrid router.
-# Memory channels... n = 4? n = 3?
+# Run the hybrid switch.
+#
+# Setting the coremask: On the Intel E5-2680's in the MAAS cluster, the
+# even-numbered cores are on socket 0 and the odd cores are on socket 1. The NIC
+# is attached to socket 1. So we need to only use the odd cores.
+#
+# Setting the memory channels: n = 4? n = 3?
 # http://dpdk-guide.gitlab.io/dpdk-guide/tuning/memory.html
 # http://dpdk.org/doc/guides/nics/mlx4.html
-sudo nice -n -20 click --dpdk -l 0-39 -n 4 --  /tmp/hybrid-switch.click
+sudo nice -n -20 \
+     click --dpdk -l 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39 -n 4 -- \
+         /tmp/hybrid-switch.click
