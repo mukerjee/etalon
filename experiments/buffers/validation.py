d6118270b88db27afed713275341fe5e573522bf
Christopher Canel <c.canel@icloud.com>
2019-06-17 02:53:44 +0000

    Change validation experiment

--- experiments/buffers/validation.py
+++ experiments/buffers/validation.py
@@ -21,13 +21,9 @@ for config in configs:
 
     settings = {'flows': []}
     settings['flows'].append({'src': 'r1', 'dst': 'r2'})
-    settings['flows'].append({'src': 'r2', 'dst': 'r1'})
-    settings['flows'].append({'src': 'r3', 'dst': 'r4'})
-    settings['flows'].append({'src': 'r4', 'dst': 'r3'})
-    settings['flows'].append({'src': 'r5', 'dst': 'r6'})
-    settings['flows'].append({'src': 'r6', 'dst': 'r5'})
-    settings['flows'].append({'src': 'r7', 'dst': 'r8'})
-    settings['flows'].append({'src': 'r8', 'dst': 'r7'})
+    settings['flows'].append({'src': 'r2', 'dst': 'r1')}
+    settings['flows'].append({'src': 'r2', 'dst': 'r3'})
+    settings['flows'].append({'src': 'r3', 'dst': 'r2'})
     flowgrind(settings)
 
 finishExperiment()
