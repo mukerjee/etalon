a545e825b21848fac9f5732e9a0b84e01a8124ae
Christopher Canel <c.canel@icloud.com>
2019-06-25 21:23:05 +0000

    Integrate retries into ns_run()

--- rpycd/rpycd.py
+++ rpycd/rpycd.py
@@ -43,37 +43,39 @@ class EtalonService(rpyc.Service):
     def exposed_run_host(self, my_id, my_cmd):
         return self.call(DOCKER_EXEC.format(id=my_id, cmd=my_cmd))
 
-    # run on the physical host but in a container's namespace
-    def exposed_ns_run(self, my_id, my_cmd):
-        my_pid = self.call(DOCKER_GET_PID.format(id=my_id)).split()[0].strip()
-        return self.call(NS_RUN.format(pid=my_pid, cmd=my_cmd))
+    # Run on the physical host but in a container's namespace. If "timeout_s" is greater than 0,
+    # then attempt "my_cmd" every "interval_s" seconds until it suceeds, or until "timeout_s"
+    # seconds have elapsed.
+    def exposed_ns_run(self, my_id, my_cmd, timeout_s=0, interval_s=0):
+        assert timeout_s >= 0, "\"timeout_s\" must >= 0, but is: {}".format(timeout_s)
+        assert interval_s >= 0, "\"interval_s\" must >= 0, but is: {}".format(interval_s)
 
-    # Attempt "cmd" every "interval_s" seconds until it suceeds, or until "timeout_s" seconds have
-    # elapsed.
-    def exposed_try_ns_run(self, my_id, cmd, timeout_s=300, interval_s=1):
+        my_pid = self.call(DOCKER_GET_PID.format(id=my_id)).split()[0].strip()
+        full_cmd = NS_RUN.format(pid=my_pid, cmd=my_cmd)
         start_s = time.time() * 1000
         current_s = start_s
-        count = 0
+        end_s = start_s + timeout_s
         once = False
+        count = 0
         while (not once) or (current_s - start_s < timeout_s):
-            count += 1
             if not once:
                 once = True
             try:
-                return self.exposed_ns_run(my_id, cmd)
-            except:
-                self.log("Trying \"{}\" again...".format(cmd))
-            if current_s + interval_s > start_s + timeout_s:
-                # Only sleep if we have at least one interval left.
+                return self.call(full_cmd)
+            except CalledProcessError:
+                self.log("Will try \"{}\" again".format(cmd))
+            count += 1
+            if current_s + interval_s > end_s:
+                # If there is at least one interval remaining, then sleep for interval_s seconds.
                 time.sleep(interval_s)
             current_s = time.time() * 1000
         raise RuntimeError(
-            "Command \"{}\" did not complete correctly after {} attempts in {} seconds!".format(
+            "Command \"{}\" did not complete successfully after {} attempts in {} seconds!".format(
                 cmd, count))
 
     # run on a physical host
-    def exposed_run(self, my_cmd):
-        return self.call(cmd=my_cmd)
+    def exposed_run(self, cmd):
+        return self.call(cmd)
 
 
 if __name__ == '__main__':
