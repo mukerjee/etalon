7126cd2d80037b03fcadd5a39e5a1f57982cd991
Christopher Canel <c.canel@icloud.com>
2019-07-03 20:06:25 +0000

    Refactor OFED and DPDK installation
    Install OFED using "--dpdk --upstream-libs". Compile DPDK manually
    instead of installing via apt.

--- bin/switch_install.sh
+++ bin/switch_install.sh
@@ -4,6 +4,8 @@
 
 set -o errexit
 
+DPDK_VERSION=17.11.6
+
 # Validate.
 if [ ! -d $HOME/etalon ]; then
     echo "Error: Etalon repo not located at \"$HOME/etalon\"!"
@@ -30,7 +32,15 @@ git submodule update --init
 # Mellanox DPDK.
 # http://www.mellanox.com/related-docs/prod_software/MLNX_DPDK_Quick_Start_Guide_v16.11_2.3.pdf
 echo "Installing Mellanox DPDK..."
-sudo apt install -y dpdk dpdk-dev
+# sudo apt install -y libnuma-dev libibverbs-dev libelf-dev
+cd $HOME
+DPDK_TAR=dpdk-$DPDK_VERSION.tar.xz
+wget http://fast.dpdk.org/rel/$DPDK_TAR
+tar xf $DPDK_TAR
+DPDK_DIR=$HOME/dpdk-stable-$DPDK_VERSION
+sed -i -r 's/CONFIG_RTE_LIBRTE_MLX4_PMD=n/CONFIG_RTE_LIBRTE_MLX4_PMD=y/' $DPDK_DIR/config/common_base
+cd $DPDK_DIR
+make -j`nproc` install T=x86_64-native-linuxapp-gcc
 
 # Huge pages. http://dpdk.org/doc/guides/linux_gsg/sys_reqs.html
 echo "Setting up huge pages..."
@@ -47,10 +57,10 @@ echo 'nodev /mnt/huge_1GB hugetlbfs pagesize=1GB 0 0' | sudo tee -a /etc/fstab
 # RTE_SDK location.
 echo "Setting RTE_SDK location..."
 echo "" >> $HOME/.bashrc
-echo "export RTE_SDK=/usr/share/dpdk" >> $HOME/.bashrc
-echo "export RTE_TARGET=x86_64-default-linuxapp-gcc" >> $HOME/.bashrc
-export RTE_SDK=/usr/share/dpdk
-export RTE_TARGET=x86_64-default-linuxapp-gcc
+echo "export RTE_SDK=$DPDK_DIR" >> $HOME/.bashrc
+echo "export RTE_TARGET=x86_64-native-linuxapp-gcc" >> $HOME/.bashrc
+export RTE_SDK=$DPDK_DIR
+export RTE_TARGET=x86_64-native-linuxapp-gcc
 
 # Click.
 echo "Installing Click..."
