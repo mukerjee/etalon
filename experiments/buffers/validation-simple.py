9557c6495eecedd0666aee0d785cd327e24ae607
Christopher Canel <c.canel@icloud.com>
2019-07-04 13:40:38 +0000

    Clean up validation-simple.py

--- experiments/buffers/validation-simple.py
+++ experiments/buffers/validation-simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #
-# A simple configuration that sets of the testbed, runs the Solstice scheduler,
-# and exits, allowing the user to play around manually.
+# A simple configuration that sets up the testbed, launches flowgrindd on the
+# hosts, runs the Solstice scheduler, and exits, allowing the user to play
+# around manually.
 
 import sys
 sys.path.insert(0, '/etalon/experiments')
@@ -10,14 +11,13 @@ from click_common import setConfig
 
 initializeExperiment('flowgrindd')
 
-configs = [{'type': 'normal'}]
+config = {
+    'type': 'normal',
+    'buffer_size': 128,
+    'divert_acks': True
+}
 
-for c in configs:
-    c['buffer_size'] = 128
-    c['divert_acks'] = True
-
-for config in configs:
-    print '--- running test type %s...' % config['type']
-    print '--- setting switch buffer size to %d...' % config['buffer_size']
-    setConfig(config)
-    print '--- done...'
+print '--- running test type %s...' % config['type']
+print '--- setting switch buffer size to %d...' % config['buffer_size']
+setConfig(config)
+print '--- done...'
