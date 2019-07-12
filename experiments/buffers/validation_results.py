81982d51737d166ea61d9c41fcfe13cad9533277
Christopher Canel <c.canel.icloud.com>
2019-07-12 01:15:46 +0000

    Fix style in validation_results.py

--- experiments/buffers/validation_results.py
+++ experiments/buffers/validation_results.py
@@ -12,10 +12,9 @@ if __name__ == '__main__':
     if not os.path.isdir(sys.argv[1]):
         print 'first arg must be dir'
         sys.exit(-1)
-    parse_validation_log(sys.argv[1], glob.glob(sys.argv[1] +
-                                                '/*-validation-no_circuit-*-'
-                                                'flowgrind.txt'))
-    
-    parse_validation_log(sys.argv[1], glob.glob(sys.argv[1] +
-                                                '/*-validation-circuit-*-'
-                                                'flowgrind.txt'))
+    parse_validation_log(
+        sys.argv[1],
+        glob.glob(sys.argv[1] + '/*-validation-no_circuit-*-flowgrind.txt'))
+    parse_validation_log(
+        sys.argv[1],
+        glob.glob(sys.argv[1] + '/*-validation-circuit-*-flowgrind.txt'))
