9df445a86be13e9a1d254ac16d9fc07491f366ab
Christopher Canel <c.canel.icloud.com>
2019-07-15 22:25:27 +0000

    Add main guard to buffers_cc.py

--- experiments/buffers/buffers_cc.py
+++ experiments/buffers/buffers_cc.py
@@ -14,31 +14,33 @@ CCS = ['reno', 'cubic', 'retcp', 'dctcp', 'bbr', 'bic', 'cdg', 'highspeed',
        'htcp', 'hybla', 'illinois', 'lp', 'nv', 'scalable', 'vegas', 'veno',
        'westwood', 'yeah']
 
-initializeExperiment('flowgrindd')
 
-# Total number of experiments.
-TOT = len(CCS) * len(buffer_common.CONFIGS)
-CNT = 0
-for cc in CCS:
-    for config in buffer_common.CONFIGS:
-        CNT += 1
+if __name__ == "__main__":
+    initializeExperiment('flowgrindd')
 
-        # Make a copy so that the if-statement below does not trigger after the
-        # first CC mode.
-        config_c = copy.deepcopy(config)
+    # Total number of experiments.
+    TOT = len(CCS) * len(buffer_common.CONFIGS)
+    CNT = 0
+    for cc in CCS:
+        for config in buffer_common.CONFIGS:
+            CNT += 1
 
-        if 'cc' in config_c:
-            # Skip the reTCP experiments because they will be run anyway when
-            # cc == 'retcp'.
-            continue
+            # Make a copy so that the if-statement below does not trigger after the
+            # first CC mode.
+            config_c = copy.deepcopy(config)
 
-        config_c['cc'] = cc
-        print '--- running test type %s...' % config_c['type']
-        print '--- using CC mode %s...' % config_c['cc']
-        print '--- setting switch buffer size to %d...' % config_c['buffer_size']
-        setConfig(config_c)
-        print '--- done...'
-        print '--- experiment %d of %d' % (CNT, TOT)
-        flowgrind(settings={'flows': [{'src': 'r1', 'dst': 'r2'}]})
+            if 'cc' in config_c:
+                # Skip the reTCP experiments because they will be run anyway when
+                # cc == 'retcp'.
+                continue
 
-finishExperiment()
+            config_c['cc'] = cc
+            print '--- running test type %s...' % config_c['type']
+            print '--- using CC mode %s...' % config_c['cc']
+            print '--- setting switch buffer size to %d...' % config_c['buffer_size']
+            setConfig(config_c)
+            print '--- done...'
+            print '--- experiment %d of %d' % (CNT, TOT)
+            flowgrind(settings={'flows': [{'src': 'r1', 'dst': 'r2'}]})
+
+    finishExperiment()
