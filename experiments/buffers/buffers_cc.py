edb8e43327a15716c24be9fa270e6e635343612d
Christopher Canel <c.canel.icloud.com>
2019-07-16 15:24:15 +0000

    Clean up buffers_cc.py

--- experiments/buffers/buffers_cc.py
+++ experiments/buffers/buffers_cc.py
@@ -2,45 +2,45 @@
 
 import copy
 import sys
-sys.path.insert(0, '/etalon/experiments')
+sys.path.insert(0, "/etalon/experiments")
 
 import buffer_common
-from click_common import setConfig
-from common import initializeExperiment, finishExperiment, flowgrind
+import click_common
+import common
 
 # All available CC modules. Found by:
 #     sudo sysctl net.ipv4.tcp_available_congestion_control
-CCS = ['reno', 'cubic', 'retcp', 'dctcp', 'bbr', 'bic', 'cdg', 'highspeed',
-       'htcp', 'hybla', 'illinois', 'lp', 'nv', 'scalable', 'vegas', 'veno',
-       'westwood', 'yeah']
+CCMS = ["reno", "cubic", "retcp", "dctcp", "bbr", "bic", "cdg", "highspeed",
+        "htcp", "hybla", "illinois", "lp", "nv", "scalable", "vegas", "veno",
+        "westwood", "yeah"]
+
+
+def main():
+    common.initializeExperiment("flowgrindd")
+
+    # Skip the reTCP experiments because they will be run anyway when
+    # cc == "retcp".
+    cnfs = [cnf for cnf in buffer_common.CONFIGS if "cc" not in cnf]
+    # Total number of experiments.
+    tot = len(CCMS) * len(cnfs)
+    cnt = 0
+    for ccm in CCMS:
+        for cnf in cnfs:
+            cnt += 1
+
+            # Make a copy to avoid modifying the original configuration object.
+            cnf_c = copy.deepcopy(cnf)
+            cnf_c["cc"] = ccm
+            print("--- running test type {}...".format(cnf_c["type"]))
+            print("--- using CC mode {}...".format(cnf_c["cc"]))
+            print("--- setting switch buffer size to {}...".format(cnf_c["buffer_size"]))
+            click_common.setConfig(cnf_c)
+            print("--- done...")
+            print("--- experiment {} of {}".format(cnt, tot))
+            common.flowgrind(settings={"flows": [{"src": "r1", "dst": "r2"}]})
+
+    common.finishExperiment()
 
 
 if __name__ == "__main__":
-    initializeExperiment('flowgrindd')
-
-    # Total number of experiments.
-    TOT = len(CCS) * len(buffer_common.CONFIGS)
-    CNT = 0
-    for cc in CCS:
-        for config in buffer_common.CONFIGS:
-            CNT += 1
-
-            # Make a copy so that the if-statement below does not trigger after the
-            # first CC mode.
-            config_c = copy.deepcopy(config)
-
-            if 'cc' in config_c:
-                # Skip the reTCP experiments because they will be run anyway when
-                # cc == 'retcp'.
-                continue
-
-            config_c['cc'] = cc
-            print '--- running test type %s...' % config_c['type']
-            print '--- using CC mode %s...' % config_c['cc']
-            print '--- setting switch buffer size to %d...' % config_c['buffer_size']
-            setConfig(config_c)
-            print '--- done...'
-            print '--- experiment %d of %d' % (CNT, TOT)
-            flowgrind(settings={'flows': [{'src': 'r1', 'dst': 'r2'}]})
-
-    finishExperiment()
+    main()
