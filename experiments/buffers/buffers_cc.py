38906e2c1bc47bade6f75d384dbec6bce39caee4
Christopher Canel <c.canel@icloud.com>
2019-08-30 14:48:41 +0000

    Increase exp dur to 5 s

--- experiments/buffers/buffers_cc.py
+++ experiments/buffers/buffers_cc.py
@@ -17,7 +17,7 @@ import python_config
 
 
 # Flowgrind flow duration, in seconds.
-DUR_S = 2
+DUR_S = 5
 # Fixed schedule: 2000 us pause, 20 us circuit night, 180 us circuit day.
 FIXED = "2 40400 -1/-1/-1 3600 1/2/0"
 
