6ce5f29bc5d0b1bbc9722633bbc8ba09f008cbf5
Christopher Canel <c.canel@icloud.com>
2019-09-15 22:22:25 +0000

    Clean up validation exps

--- experiments/buffers/validation.py
+++ experiments/buffers/validation.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
 
+from os import path
 import sys
-sys.path.insert(0, '/etalon/experiments')
-from common import initializeExperiment, finishExperiment, flowgrind
+# Directory containing this program.
+PROGDIR = path.dirname(path.realpath(__file__))
+# For click_common and common.
+sys.path.insert(0, path.join(PROGDIR, ".."))
+
 from click_common import setConfig
+from common import initializeExperiment, finishExperiment, flowgrind
 
-initializeExperiment('flowgrindd')
 
-configs = [{'type': 'no_circuit'}, {'type': 'circuit'}]
+def main():
+    initializeExperiment('flowgrindd')
 
-for c in configs:
-    c['buffer_size'] = 128
-    c['divert_acks'] = True
+    configs = [{'type': 'no_circuit'}, {'type': 'circuit'}]
+    for c in configs:
+        c['cc'] = 'cubic'
+        c['buffer_size'] = 128
+        c['divert_acks'] = True
+    for config in configs:
+        print '--- running test type %s...' % config['type']
+        print '--- setting switch buffer size to %d...' % config['buffer_size']
+        setConfig(config)
+        print '--- done...'
 
-for config in configs:
-    print '--- running test type %s...' % config['type']
-    print '--- setting switch buffer size to %d...' % config['buffer_size']
-    setConfig(config)
-    print '--- done...'
+        settings = {'flows': []}
+        # A ring: 1->2, 2->3, 3->1. Mimics DEFAULT_CIRCUIT_CONFIG.
+        settings['flows'].append({'src': 'r1', 'dst': 'r2'})
+        settings['flows'].append({'src': 'r2', 'dst': 'r3'})
+        settings['flows'].append({'src': 'r3', 'dst': 'r1'})
+        flowgrind(settings)
 
-    settings = {'flows': []}
-    # A ring: 1->2, 2->3, 3->1. Mimics DEFAULT_CIRCUIT_CONFIG.
-    settings['flows'].append({'src': 'r1', 'dst': 'r2'})
-    settings['flows'].append({'src': 'r2', 'dst': 'r3'})
-    settings['flows'].append({'src': 'r3', 'dst': 'r1'})
-    flowgrind(settings)
+    finishExperiment()
 
-finishExperiment()
+
+if __name__ == "__main__":
+    main()
