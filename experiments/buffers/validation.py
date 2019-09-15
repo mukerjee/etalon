025f887b5f12789bfaf0838c42e57fc3f3d342b1
Christopher Canel <c.canel@icloud.com>
2019-09-15 19:04:48 -0400

    Unify validation experiments

--- experiments/buffers/validation.py
+++ experiments/buffers/validation.py
@@ -7,32 +7,30 @@ PROGDIR = path.dirname(path.realpath(__file__))
 # For click_common and common.
 sys.path.insert(0, path.join(PROGDIR, ".."))
 
-from click_common import setConfig
-from common import initializeExperiment, finishExperiment, flowgrind
+import click_common
+import common
 
 
 def main():
-    initializeExperiment('flowgrindd')
+    common.initializeExperiment("flowgrindd")
 
-    configs = [{'type': 'no_circuit'}, {'type': 'circuit'}]
-    for c in configs:
-        c['cc'] = 'cubic'
-        c['buffer_size'] = 128
-        c['divert_acks'] = True
-    for config in configs:
-        print '--- running test type %s...' % config['type']
-        print '--- setting switch buffer size to %d...' % config['buffer_size']
-        setConfig(config)
-        print '--- done...'
+    for typ in ["no_circuit", "circuit", "strobe"]:
+        cnf = {"type": typ,
+               "cc": "cubic",
+               "buffer_size": 128,
+               "divert_acks": True}
+        click_common.setConfig(cnf)
+        print("--- config:\n{}".format(cnf))
 
-        settings = {'flows': []}
         # A ring: 1->2, 2->3, 3->1. Mimics DEFAULT_CIRCUIT_CONFIG.
-        settings['flows'].append({'src': 'r1', 'dst': 'r2'})
-        settings['flows'].append({'src': 'r2', 'dst': 'r3'})
-        settings['flows'].append({'src': 'r3', 'dst': 'r1'})
-        flowgrind(settings)
+        click_common.flowgrind(settings={
+            "flows": [
+                {"src": "r1", "dst": "r2"},
+                {"src": "r2", "dst": "r3"},
+                {"src": "r3", "dst": "r1"}]})
 
-    finishExperiment()
+
+    common.finishExperiment()
 
 
 if __name__ == "__main__":
--- experiments/buffers/validation_results.py
+++ experiments/buffers/validation_results.py
@@ -12,16 +12,17 @@ import parse_logs
 
 
 def main():
+    assert len(sys.argv) == 2, "Expected one argument: experiment dir"
     edr = sys.argv[1]
     if not path.isdir(edr):
         print("The first argument must be a directory, but is: {}".format(edr))
         sys.exit(-1)
 
-    parse_logs.parse_validation_log(
-        glob.glob(path.join(edr, '*-validation-no_circuit-*-flowgrind.txt'))[0])
-    parse_logs.parse_validation_log(
-        glob.glob(path.join(edr, '*-validation-circuit-*-flowgrind.txt'))[0])
+    for ptn in ["*-no_circuit-*-flowgrind.txt",
+                "*-circuit-*-flowgrind.txt",
+                "*-strobe-*-flowgrind.txt"]:
+        parse_logs.parse_validation_log(glob.glob(path.join(edr, ptn))[0])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
