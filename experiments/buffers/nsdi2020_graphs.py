0b76d80826954b2e3ae68d4396ada48e847e5ec0
Christopher Canel <c.canel@icloud.com>
2019-09-17 14:57:57 -0400

    Refactor zoom graph generation

--- experiments/buffers/nsdi2020_graphs.py
+++ experiments/buffers/nsdi2020_graphs.py
@@ -208,36 +208,29 @@ def main():
 
     # (6.1) With and without inset.
     for ins in [DYN_INS, None]:
-        sg.seq(
-            name="6-1_seq-dyn-{}-chunk{}{}".format(
-                CHOSEN_TCP, CHUNK_IDX, "_inset" if ins is not None else ""),
-            edr=edr,
-            odr=odr,
-            ptn=DYN_PTN.format("*", CHOSEN_TCP),
-            key_fnc=lambda fn: int(round(float(fn.split("-")[6])
-                                         / python_config.TDF)),
-            dur=1200,
-            ins=ins,
-            flt=(lambda idx, label, order=ORDER_DYN: \
-                 label.strip(" $\mu$s") in order),
-            order=ORDER_DYN,
-            chunk_idx=CHUNK_IDX)
-
-    # (6.1) With zoom.
-    sg.seq(
-        name="6-1_seq-dyn-{}-chunk{}_zoom".format(CHOSEN_TCP, CHUNK_IDX),
-        edr=edr,
-        odr=odr,
-        ptn=DYN_PTN.format("*", CHOSEN_TCP),
-        key_fnc=lambda fn: int(round(float(fn.split("-")[6])
-                                     / python_config.TDF)),
-        dur=1200,
-        ins=ins,
-        flt=(lambda idx, label, order=ORDER_DYN: \
-             label.strip(" $\mu$s") in order),
-        order=ORDER_DYN,
-        xlm=XLM_ZOOM,
-        ylm=YLM_ZOOM)
+        for chunk_idx in [CHUNK_IDX, None]:
+            for xlm_zoom, ylm_zoom in [(XLM_ZOOM, YLM_ZOOM), (None, None)]:
+                if ins is None or xlm_zoom is None:
+                    sg.seq(
+                        name="6-1_seq-dyn-{}{}{}{}".format(
+                            CHOSEN_TCP,
+                            ("-chunk{}".format(chunk_idx)
+                             if chunk_idx is not None else ""),
+                            "_inset" if ins is not None else "",
+                            "_zoom" if xlm_zoom is not None else ""),
+                        edr=edr,
+                        odr=odr,
+                        ptn=DYN_PTN.format("*", CHOSEN_TCP),
+                        key_fnc=lambda fn: int(round(float(fn.split("-")[6])
+                                                     / python_config.TDF)),
+                        dur=1200,
+                        ins=ins,
+                        flt=(lambda idx, label, order=ORDER_DYN: \
+                             label.strip(" $\mu$s") in order),
+                        order=ORDER_DYN,
+                        xlm=xlm_zoom,
+                        ylm=ylm_zoom,
+                        chunk_idx=chunk_idx)
 
     # (6.2)
     buffers_graphs.util(
