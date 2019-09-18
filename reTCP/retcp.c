a189c0dc536f0c9969a4c5cd6409cbd60403c36f
Christopher Canel <c.canel@icloud.com>
2019-09-18 15:10:33 +0000

    Disable logs in reTCP

--- reTCP/retcp.c
+++ reTCP/retcp.c
@@ -27,7 +27,7 @@ static void retcp_init(struct sock *sk)
   ca->have_circuit = 0;
   ca->jumped = 0;
 
-  printk(KERN_DEBUG "retcp_init\n");
+  // printk(KERN_DEBUG "retcp_init\n");
 }
 
 static void retcp_in_ack(struct sock *sk, u32 flags)
@@ -42,18 +42,18 @@ static void retcp_cong_avoid(struct sock *sk, u32 ack, u32 acked)
   struct tcp_sock *tp = tcp_sk(sk);
   tcp_reno_cong_avoid(sk, ack, acked);
 
-  printk(KERN_DEBUG "\nretcp_cong_avoid\n");
-  printk(KERN_DEBUG "cwnd: %d\n", tp->snd_cwnd);
-  printk(KERN_DEBUG "have_circuit: %d\n", ca->have_circuit);
-  printk(KERN_DEBUG "jumped: %d\n", ca->jumped);
+  // printk(KERN_DEBUG "\nretcp_cong_avoid\n");
+  // printk(KERN_DEBUG "cwnd: %d\n", tp->snd_cwnd);
+  // printk(KERN_DEBUG "have_circuit: %d\n", ca->have_circuit);
+  // printk(KERN_DEBUG "jumped: %d\n", ca->jumped);
 
   if (ca->have_circuit && !ca->jumped) {
-    printk(KERN_DEBUG "retcp 1");
+    // printk(KERN_DEBUG "retcp 1");
     tp->snd_cwnd *= jump_up;
     ca->jumped = 1;
   }
   if (!ca->have_circuit && ca->jumped) {
-    printk(KERN_DEBUG "retcp 2");
+    // printk(KERN_DEBUG "retcp 2");
     tp->snd_cwnd /= jump_down;
     ca->jumped = 0;
   }
