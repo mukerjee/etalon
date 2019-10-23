e985f72e5f2e8fe80d41f6bc08794322ad5c34cf
Christopher Canel <c.canel@icloud.com>
2019-10-23 18:25:45 +0000

    Clean up rpycd.py

--- rpycd/README.md
+++ rpycd/README.md
@@ -1,2 +1,5 @@
 # RPYCD (RPyC daemon)
-The RPyC daemon runs on each non-switch node in order for the switch node to run commands remotely on the other nodes. It is installed as a service using systemctl in ../bin/node_install.sh
+
+The RPyC daemon runs on each non-switch node in order for the switch
+node to run commands remotely on the other nodes. It is installed as a
+service using systemctl in `etalon/bin/node_install.sh`.
--- rpycd/rpycd.py
+++ rpycd/rpycd.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
 import datetime
-import socket
-import time
-from subprocess import check_output, CalledProcessError
+import subprocess
 import sys
+import time
 
 import rpyc
-from rpyc.utils.server import ThreadedServer
+from rpyc.utils import server
 
 sys.path.insert(0, '/etalon/etc')
-from python_config import RPYC_PORT, SWITCH_CONTROL_IP
-
-DOCKER_EXEC = 'sudo docker exec -t h{id} {cmd}'
-DOCKER_GET_PID = "sudo docker inspect --format '{{{{.State.Pid}}}}' h{id}"
-NS_RUN = 'sudo nsenter -t {pid} -n {cmd}'
-SELF_ID = int(socket.gethostname().split('.')[0][-1])
+import python_config
 
 
 class EtalonService(rpyc.Service):
@@ -25,66 +19,75 @@ class EtalonService(rpyc.Service):
             erf.write("{}: {}\n".format(
                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg))
 
-    # drops all connections that aren't from the switch
     def on_connect(self, conn):
-        if conn._config['endpoints'][1][0] != SWITCH_CONTROL_IP:
-            raise AssertionError("rpyc connection not from switch")
+        """ Drops all connections that aren't from the switch. """
+        assert (conn._config['endpoints'][1][0] == \
+                python_config.SWITCH_CONTROL_IP), \
+                "rpyc connection not from switch"
 
-    def call(self, cmd, check_rc=True):
+    def run(self, cmd, check_rc=True):
         try:
             self.log("Running cmd: {}".format(cmd))
-            output = check_output(cmd, shell=True)
+            output = subprocess.check_output(cmd, shell=True)
             # For prettiness, only print a newline if there was some output.
-            output_msg = "\n{}".format(output.strip()) if output != "" else ""
-            self.log("Finished cmd: {} , output:{}".format(cmd, output_msg))
+            self.log("Finished cmd: {} , output:{}".format(
+                cmd, "\n{}".format(output.strip()) if output != "" else ""))
             return output
-        except CalledProcessError as exp:
+        except subprocess.CalledProcessError as exp:
             if check_rc:
-                self.log("cmd: {} , output: Error\nreturncode: {}\noutput:\n{}".format(
-                    cmd, exp.returncode, exp.output))
+                self.log(
+                    ("cmd: {} , output: Error\nreturncode: "
+                     "{}\noutput:\n{}").format(cmd, exp.returncode, exp.output))
                 raise exp
 
-    # run on a container
     def exposed_run_host(self, my_id, my_cmd):
-        return self.call(DOCKER_EXEC.format(id=my_id, cmd=my_cmd))
+        """ Run on a container. """
+        return self.run(python_config.DOCKER_EXEC.format(id=my_id, cmd=my_cmd))
 
-    # Run on the physical host but in a container's namespace. If "timeout_s" is greater than 0,
-    # then attempt "my_cmd" every "interval_s" seconds until it suceeds, or until "timeout_s"
-    # seconds have elapsed.
     def exposed_ns_run(self, my_id, my_cmd, timeout_s=0, interval_s=0):
-        assert timeout_s >= 0, "\"timeout_s\" must be >= 0, but is: {}".format(timeout_s)
-        assert interval_s >= 0, "\"interval_s\" must be >= 0, but is: {}".format(interval_s)
+        """
+        Run on the physical host but in a container's namespace. If "timeout_s"
+        is greater than 0, then attempt "my_cmd" every "interval_s" seconds
+        until it suceeds, or until "timeout_s" seconds have elapsed.
+        """
+        assert timeout_s >= 0, \
+            "\"timeout_s\" must be >= 0, but is: {}".format(timeout_s)
+        assert interval_s >= 0, \
+            "\"interval_s\" must be >= 0, but is: {}".format(interval_s)
 
-        my_pid = self.call(DOCKER_GET_PID.format(id=my_id)).split()[0].strip()
-        full_cmd = NS_RUN.format(pid=my_pid, cmd=my_cmd)
+        my_pid = self.run(cmd=python_config.DOCKER_GET_PID.format(
+            id=my_id)).split()[0].strip()
+        full_cmd = python_config.NS_RUN.format(pid=my_pid, cmd=my_cmd)
         start_s = time.time()
         current_s = start_s
         end_s = start_s + timeout_s
         once = False
         count = 0
         while (not once) or (current_s - start_s < timeout_s):
-            if not once:
-                once = True
+            once = True
             try:
-                return self.call(full_cmd)
-            except CalledProcessError:
+                return self.run(full_cmd)
+            except subprocess.CalledProcessError:
                 count += 1
-                self.log(("Will try command \"{}\" on host \"{}\" again (attempt #{}) in {} "
-                          "second(s).").format(my_cmd, my_id, count + 1, interval_s))
+                self.log(("Will try command \"{}\" on host \"{}\" again "
+                          "(attempt #{}) in {} second(s).").format(
+                              my_cmd, my_id, count + 1, interval_s))
             if current_s + interval_s > end_s:
-                # If there is at least one interval remaining, then sleep for interval_s seconds.
+                # If there is at least one interval remaining, then sleep for
+                # interval_s seconds.
                 time.sleep(interval_s)
             current_s = time.time()
-        msg = ("Command \"{}\" on host \"{}\" did not complete successfully after {} attempt(s) in "
-               "{} seconds!").format(my_cmd, my_id, count, timeout_s)
+        msg = ("Command \"{}\" on host \"{}\" did not complete successfully "
+               "after {} attempt(s) in {} seconds!").format(
+                   my_cmd, my_id, count, timeout_s)
         self.log(msg)
         raise RuntimeError(msg)
 
-    # run on a physical host
     def exposed_run(self, cmd):
-        return self.call(cmd)
+        """ Run on a physical host. """
+        return self.run(cmd)
 
 
 if __name__ == '__main__':
-    ThreadedServer(
-        EtalonService, port=RPYC_PORT, protocol_config={"allow_all_attrs": True}).start()
+    server.ThreadedServer(EtalonService, port=python_config.RPYC_PORT,
+                          protocol_config={"allow_all_attrs": True}).start()
