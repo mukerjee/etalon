0049de057c841ff8a75c491868d4524257507ed0
Christopher Canel <c.canel@icloud.com>
2019-12-11 21:31:48 +0000

    Add comments to buffers_common.py

--- experiments/buffers/buffer_common.py
+++ experiments/buffers/buffer_common.py
@@ -1,6 +1,12 @@
 
 
 def gen_static_sweep(mn, mx):
+    # Generate a sweep of configuration with different static buffer sizes.
+    #
+    # mn: minimum buffer size
+    # mx: maximum buffer size
+    #
+    # mn and mx are in terms of powers of 2 (e.g., mn = 7 -> 2^7 = 128 packets).
     return [{
         # Enable the hybrid switch's packet log. This should already be enabled
         # by default.
@@ -11,6 +17,14 @@ def gen_static_sweep(mn, mx):
 
 
 def gen_resize_sweep(mn, mx, dl):
+    # Generate a sweep of configurations with different amounts of dynamic
+    # buffer resizing.
+    #
+    # mn: minimum resizing duration
+    # mx: maximum resizing duration
+    # dl: delta by which to increase the resizing duration.
+    #
+    # mn, mx, and dl are in terms of microseconds.
     return [{
         # Enable the hybrid switch's packet log. This should already be enabled
         # by default.
