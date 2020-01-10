28703082788ff19cc804eefeb1aed8493da0924e
Christopher Canel <c.canel@icloud.com>
2020-01-10 17:12:48 -0500

    Do not remove crond.reboot file

--- bin/install.sh
+++ bin/install.sh
@@ -33,7 +33,6 @@ sudo ln -sfv "$HOME/etalon" /
 # Always run tuning on boot.
 (crontab -l 2>/dev/null; echo "@reboot sleep 60 && /etalon/bin/tune.sh && /etalon/bin/retcp_install.sh") | \
     crontab -
-# sudo rm -fv /var/run/crond.reboot
 
 # Change the hostname.
 OLD_HOSTNAME=$(hostname)
