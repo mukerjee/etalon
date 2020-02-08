ef5568fb64589d0f41932f53499417de500dd42c
Christopher Canel <c.canel@icloud.com>
2020-02-08 21:12:00 +0000

    Add more cleanup to NSDI exps bash script

--- bin/nsdi2020.sh
+++ bin/nsdi2020.sh
@@ -4,12 +4,17 @@
 
 set -o errexit
 
-# Remove old results files that require root priviledges.
-sudo rm -fv /tmp/*click.txt
-# Remove results from aborted experiments.
-rm -fv "$HOME"/1tb/*txt
+function cleanup {
+    # Remove old results files that require root priviledges.
+    sudo rm -fv /tmp/*click.txt
+    # Remove results from aborted experiments.
+    rm -fv "$HOME"/1tb/*txt
+}
+
+cleanup
 # Flush the OS buffer cache.
 sudo sync;
 echo "1" | sudo tee /proc/sys/vm/drop_caches
 # Run experiments.
 "$HOME"/etalon/experiments/buffers/nsdi2020.py
+cleanup
